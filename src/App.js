f8c2ede7dc07536b7ccde2393370a531d97e25e6
kasi-iyo <snakeman143@gmail.com>
2020-08-25 11:48:35 +0900

    styled-componentsインストール

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios'
+import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-function App() {
+import Dashboard from './components/Dashboard'
+import Home from './components/Home'
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [user, setUser] = useState({})
-
-  useEffect(() => {
-    loginStatus()
-  }, [])
-
-  loginStatus = () => {
-    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
-      .then(response => {
-        if (response.data.logged_in) {
-          handleLogin(resposne)
-        } else {
-          handleLogout()
-        }
-      }).catch(error => console.log('apiエラー: ', error))
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
   }
-
-  handleLogin = (data) => {
-    setIsLoggedIn(true)
-    setUser(data.user)
-  }
-
-  handleLogout = () => {
-    setIsLoggedIn(faluse)
-    setUser({})
-  }
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={} />
-          <Route exact path="/login" component={} />
-          <Route exact path="/signup" component={} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
 }
-
-export default App;
