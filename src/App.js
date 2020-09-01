ef43909d310a72e118090a9bcb809e486b215a07
kasi-iyo <snakeman143@gmail.com>
2020-09-01 16:04:02 +0900

    App.js、App.css作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 
 import Home from './components/Home/Home'
 import Dashboard from './components/Dashboard'
+import Header from './components/Header/Header'
 import Login from './components/auth/Login'
 import Registration from './components/auth/Registration'
 import './App.css'
@@ -12,29 +13,35 @@ export default function App() {
   const [loggedInStatus, setLoggedInStatus] = useState(false)
   const [user, setUser] = useState({})
 
+  // エラー出力===========================================
+  const handleErrors = () => {
+    return (
+        <div>
+            <ul>
+                {loginErrors.map(error => {
+                    return <li key={error}>{error}</li>
+                })}
+            </ul>
+        </div>
+    )
+  }
+  //======================================================
+
   // 認証系の関数=======================================================================
+  // ログイン===============================
   const handleLogin = (data) => {
     setLoggedInStatus(true)
     setUser(data.user)
   }
+  //========================================
 
-  const handleLogout = () => {
-    setLoggedInStatus(false)
-    setUser({})
+  // リダイレクト===========================
+  const redirect = () => {
+    props.history.push("/")
   }
+  //========================================
 
-  const handleSuccessfulAuth = (data) => {
-    handleLogin(data)
-    data.history.push("/dashboard")
-}
-
-const handleLogoutClick = () => {
-    axios.delete("http://localhost:3001/logout", { withCredentials: true })
-        .then(response => {
-            handleLogout()
-        }).catch(error => console.log("ログアウトエラー", error))
-}
-
+  // ログインステータスの追跡===============================
   useEffect(() => {
     checkLoginStatus()
   })
@@ -53,39 +60,18 @@ const handleLogoutClick = () => {
         console.log("ログインエラー", error)
       })
   }
-  //===============================================================================
+//==========================================================
+//===============================================================================
 
   return (
     <div>
       <BrowserRouter>
-        
-        <header className="header">
-          <div className="header__top">
-            {user.nickname ? <p><span className="userstatus">{user.nickname}</span>でログイン中</p> : <p>ゲスト</p>}
-          </div>
-
-          <div className="header__bottom">
-            <ul>
-              <li className="li__link"><Link to="/">ホーム</Link></li>
-              {
-                loggedInStatus
-                  ? <button className="logout__button" onClick={handleLogoutClick}>ログアウト</button>
-                  : <ul><li><Link to="/login">ログイン</Link></li><li><Link to="/signup">新規登録</Link></li></ul>
-              }
-            </ul>
-          </div>
-        </header>
-
+        <Header />
         <Switch>
           <Route
             exact path={"/"}
             render={props => (
-              <Home
-                {...props}
-                handleLogin={handleLogin}
-                handleLogout={handleLogout}
-                loggedInStatus={loggedInStatus}
-              />
+              <Home {...props} loggedInStatus={loggedInStatus} />
             )}
           />
           <Route
@@ -94,28 +80,14 @@ const handleLogoutClick = () => {
               <Dashboard { ...props } loggedInStatus={loggedInStatus} />
             )}
           />
-          <Route 
-            exact path={"/login"}
-            render={props => (
-              <Login
-                {...props}
-                handleLogin={handleLogin}
-                handleSuccessfulAuth={handleSuccessfulAuth}
-                loggedInStatus={loggedInStatus}
-              />
-            )}
-          />
-          <Route 
-            exact path={"/signup"}
-            render={props => (
-              <Registration
-                {...props}
-                handleLogin={handleLogin}
-                handleSuccessfulAuth={handleSuccessfulAuth}
-                loggedInStatus={loggedInStatus}
-              />
-            )}
-          />
+          {!loggedInStatus && <Route exact path={"/login"} render={props => (
+              <Login {...props} handleLogin={handleLogin} redirect={redirect} handleErrors={handleErrors} loggedInStatus={loggedInStatus} />
+            )} />
+          },
+          {!loggedInStatus && <Route exact path={"/signup"} render={props => (
+              <Registration {...props} handleLogin={handleLogin} redirect={redirect} handleErrors={handleErrors} loggedInStatus={loggedInStatus} />
+            )} />
+          }
         </Switch>
       </BrowserRouter>
     </div>
