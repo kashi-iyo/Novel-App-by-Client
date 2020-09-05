6ac579302d45f4dc7b05a162ce2e61d5ee51a7ba
kasi-iyo <snakeman143@gmail.com>
2020-09-05 19:25:04 +0900

    Homeコンポーネント作成：SerieRankingコンポーネントをレンダリング

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,39 @@
-import React from 'react'
-// import axios from 'axios'
-// import { Link } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 import './Home.css'
+import SeriesRanking from './SeriesRanking/SeriesRanking'
+import Series from './Series/Series'
 
-export default function Home(props) {
+function Home(props) {
+
+    const [novelSeries, setNovelSeries] = useState([])
+    console.log(novelSeries)
+
+    useEffect(() => {
+        getNovelSeries()
+    }, [])
+
+    const getNovelSeries = () => {
+        axios.get('http://localhost:3001', { withCredentials: true })
+            .then(response => {
+                setNovelSeries(response.data.novel_series)
+            })
+            .catch(error => console.log(error))
+    }
 
     return (
         <div className="home">
-            <h1 className="home__h1">Home</h1>
+            <div className="home__ranking">
+                <SeriesRanking>
+                    {novelSeries.map(series => (
+                        <Series key={series.id} series={series} />
+                    ))}
+                </SeriesRanking>
+            </div>
         </div>
     )
 }
+
+export default Home
