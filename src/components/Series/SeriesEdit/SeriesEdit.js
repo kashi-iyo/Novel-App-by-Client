69744ed6e826422b73297c6d98d68262fa7f0510
kasi-iyo <snakeman143@gmail.com>
2020-09-07 19:42:38 +0900

    マウント／アンマウント時の処理を追加。

--- src/components/Series/SeriesEdit/SeriesEdit.js
+++ src/components/Series/SeriesEdit/SeriesEdit.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-import './SeriesEdit'
 import SeriesForm from '../SeriesForm/SeriesForm'
 
 function SeriesEdit(props) {
+    const [isMounted, setIsMounted] = useState(false)
     const [seriesTitle, setSeriesTitle] = useState("")
     const [seriesDescription, setSeriesDescription] = useState("")
 
@@ -15,13 +15,17 @@ function SeriesEdit(props) {
     useEffect(() => {
         const seriesValue = () => {
             axios.get(`http://localhost:3001/api/v1/${path}`, { withCredentials: true }).then(response => {
-                setSeriesTitle(response.data.novel_series.series_title)
-                setSeriesDescription(response.data.novel_series.series_description)
+                if (isMounted) {
+                    setSeriesTitle(response.data.novel_series.series_title)
+                    setSeriesDescription(response.data.novel_series.series_description)
+                }
             })
             .catch(error => console.log('エラー: ', error))
             }
         seriesValue()
-      }, [path])
+        setIsMounted(true)
+
+      }, [path, isMounted])
 
     return (
         <div className='seriesEdit'>
@@ -35,6 +39,8 @@ function SeriesEdit(props) {
                 setSeriesTitle={setSeriesTitle}
                 seriesDescription={seriesDescription}
                 setSeriesDescription={setSeriesDescription}
+                isMountd={isMounted}
+                setIsMounted={setIsMounted}
                 path={path}
                 button="編集を完了する"
             />
