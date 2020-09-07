22102da8534a0b013863f855a5a13e3fee3bb5fc
kasi-iyo <snakeman143@gmail.com>
2020-09-07 19:11:53 +0900

    Headerコンポーネントの編集

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,85 +1,60 @@
-import React, {useState, useEffect} from 'react'
+import React, {Fragment} from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 import './Header.css'
 
 function Header(props) {
-    const [loggedInStatus, setLoggedInStatus] = useState(false)
-    const [user, setUser] = useState({})
+    const loggedInStatus = props.loggedInStatus
+    const nickname = props.user.nickname
 
-  // ログアウト===============================
-  const handleLogout = () => {
-    setLoggedInStatus(false)
-    setUser({})
-  }
+    // リダイレクト
+    const redirect = () => {
+        props.history.push("/")
+    }
 
-const handleLogoutClick = () => {
-    axios.delete("http://localhost:3001/logout", { withCredentials: true })
-        .then(response => {
-            handleLogout()
-        }).catch(error => console.log("ログアウトエラー", error))
-}
-  // ログアウト===============================
-
-  // ログインステータスの追跡===============================
-  useEffect(() => {
-    checkLoginStatus()
-  })
-
-  const checkLoginStatus = () => {
-    axios.get("http://localhost:3001/logged_in", { withCredentials: true })
-      .then(response => {
-        if (response.data.logged_in && !loggedInStatus) {
-          setLoggedInStatus(true)
-          setUser(response.data.user)
-        } else if (!response.data.logged_in && loggedInStatus) {
-          setLoggedInStatus(false)
-          setUser({})
-        }
-      }).catch(error => {
-        console.log("ログインエラー", error)
-      })
-  }
-// ログインステータスの追跡===============================
+    const handleClick = () => {
+        axios.delete('http://localhost:3001/logout',
+            { withCredentials: true })
+          .then(response => {
+            if (response.data.logged_out) {
+              props.handleLogout()
+              redirect()
+            }
+          })
+            .catch(error => console.log(error))
+    }
 
     return (
-        <div>
-            <header className="header">
-                <div className="header__top">
-                    {user.nickname
-                        ?
-                        <div className="userstatus">
-                            <span>{user.nickname}</span>でログイン中
-                        </div>
-                        :
-                        <p className="guest">ゲスト</p>
-                    }
-                </div>
-
-                <div className="header__bottom">
-                    <ul>
-                        <li className="li__link"><Link to="/">ホーム</Link></li>
-                        <li><Link>ランキング</Link></li>
-                        <ul className="header__options">
-                            <li><Link>投稿一覧</Link></li>
-                            ／
-                            <li><Link to="/series_create">投稿する</Link></li>
-                        </ul>
-                        {
-                            loggedInStatus ?
-                            <button className="logout__button" onClick={handleLogoutClick}>
-                                ログアウト
-                            </button> :
-                            <ul>
-                                <li><Link to="/login">ログイン</Link></li>
-                                <li><Link to="/signup">新規登録</Link></li>
-                            </ul>
-                        }
-                    </ul>
-                </div>
-            </header>
-        </div>
+        <header className="home__header">
+            <div className="header__top">
+                {nickname
+                    ?
+                    <div className="userstatus">
+                        <Link className="nickname">{nickname}さん</Link>でログイン中
+                    </div>
+                    :
+                    <p className="guest">ゲスト</p>
+                }
+            </div>
+            <ul>
+                <li><Link to="/">ホーム</Link></li>
+                <li><Link>ランキング</Link></li>
+                <ul className="header__options">
+                    <li><Link>投稿一覧</Link></li>
+                    ／
+                    <li><Link to="/series_create">投稿する</Link></li>
+                </ul>
+                {
+                    loggedInStatus ?
+                        <li><Link to="/logout" onClick={handleClick}>ログアウト</Link></li> :
+                        <Fragment>
+                            <li><Link to="/login">ログイン</Link></li>
+                            <li><Link to="/signup">新規登録</Link></li>
+                        </Fragment>
+                }
+            </ul>
+        </header>
     )
 }
 
