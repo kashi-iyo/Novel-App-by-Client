66399a4d8bccc3c9bab121e0e7e9af902fc076d3
kasi-iyo <snakeman143@gmail.com>
2020-09-07 22:01:55 +0900

    非ログイン状態でシリーズ作成フォームへアクセスしようとした場合の制限

--- src/components/Series/SeriesCreate/SeriesCreate.js
+++ src/components/Series/SeriesCreate/SeriesCreate.js
@@ -1,22 +1,44 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import SeriesForm from '../SeriesForm/SeriesForm'
 
 function SeriesCreate(props) {
     const [seriesTitle, setSeriesTitle] = useState("")
     const [seriesDescription, setSeriesDescription] = useState("")
+    const loggedInStatus = props.loggedInStatus
+
+    // シリーズ作成フォーム
+    const handleValidateSeriesCreate = () => {
+        return (
+            <div className="seriesCreate">
+                <h3>シリーズを作成する</h3>
+                <SeriesForm {...props}
+                    seriesTitle={seriesTitle}
+                    setSeriesTitle={setSeriesTitle}
+                    seriesDescription={seriesDescription}
+                    setSeriesDescription={setSeriesDescription}
+                    button="作成する"
+                />
+            </div>
+        )
+    }
+
+    // ログイン画面への誘導メッセージ
+    const handleLeadingToLogin = () => {
+        return (
+            <div className="leadingToLogin">
+                <div>この機能を利用するには<Link to="/login">ログイン</Link>が必要です。</div>
+            </div>
+        )
+    }
 
     return (
-        <div className="seriesCreate">
-            <h3>シリーズを作成する</h3>
-
-            <SeriesForm {...props}
-                seriesTitle={seriesTitle}
-                setSeriesTitle={setSeriesTitle}
-                seriesDescription={seriesDescription}
-                setSeriesDescription={setSeriesDescription}
-                button="作成する"
-            />
+        <div>
+            {loggedInStatus ?
+                handleValidateSeriesCreate() :
+                handleLeadingToLogin()
+            }
         </div>
     )
 }
