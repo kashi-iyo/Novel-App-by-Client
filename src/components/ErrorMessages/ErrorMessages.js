22b8979bf2e28520dc055ec3aacd7745bd067db9
kasi-iyo <snakeman143@gmail.com>
2020-09-08 13:34:22 +0900

    ErrorMessagesコンポーネント編集。 アクセス権限エラー・非公開エラーによって表示切り替え。

--- src/components/ErrorMessages/ErrorMessages.js
+++ src/components/ErrorMessages/ErrorMessages.js
@@ -3,20 +3,49 @@ import React from 'react'
 import './ErrorMessages.css'
 
 function ErrorMessages(props) {
+    const releaseErrors = props.releaseErrors
     const errors = props.errors
 
-    const handleErrors = (errors) => {
+    // 非公開エラー
+    const handleReleaseErrors = (releaseErrors) => {
         return (
-          <div className="Errors">
-            {errors}
-            <p>3秒後にページがリダイレクトされます。</p>
-          </div>
+            <div className="Errors">
+                <p className="Errors__release">{releaseErrors}</p>
+                <p>3秒後に、ホーム画面へリダイレクトされます。</p>
+            </div>
         )
+    }
+
+    // アクセスエラー
+    const handleAccessErrors = (errors) => {
+        return (
+            <div className="Errors">
+                <p className="Errors__access">{errors}</p>
+                <p>3秒後に、ログインページへリダイレクトされます。</p>
+            </div>
+        )
+    }
+
+    // アクセスエラーor非公開エラーにより条件分岐
+    const errorsRender = () => {
+        if (errors) {
+            return (
+                <div>
+                    {handleAccessErrors(errors)}
+                </div>
+            )
+        } else if (releaseErrors) {
+            return (
+                <div>
+                    {handleReleaseErrors(releaseErrors)}
+                </div>
+            )
+        }
     }
 
     return (
         <div>
-            {handleErrors(errors)}
+            {errorsRender(errors)}
         </div>
     )
 }
