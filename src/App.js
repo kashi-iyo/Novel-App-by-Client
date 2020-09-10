480d7da4e89b0cad2108dd838aea5ec0fd253ba3
kasi-iyo <snakeman143@gmail.com>
2020-09-10 13:11:34 +0900

    Loginコンポーネント編集。 ログインしている状態でのログインページへのアクセスを制御。

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 
 import Home from './components/Home/Home'
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,20 +1,28 @@
 import React, {useState} from 'react'
 import axios from 'axios'
-// import { Link } from 'react-router-dom'
 
 function Login(props) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [errors, setErrors] = useState("")
+    const handleLogin = props.handleLogin
+    const loggedInStatus = props.loggedInStatus
     const user = {user: {email: email, password: password}}
+
+    // リダイレクト
+    const redirect = () => {
+        props.history.push("/")
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
-        axios.post('http://localhost:3001/login', user, { withCredentials: true })
+        axios.post('http://localhost:3001/login',
+            user,
+            { withCredentials: true }
+        )
             .then(response => {
                 if (response.data.logged_in) {
-                    props.handleLogin(response)
+                    handleLogin(response)
                     redirect()
                 } else {
                     setErrors(response.data.errors)
@@ -23,9 +31,15 @@ function Login(props) {
             .catch(error => console.log("エラー: ", error))
     }
 
-    const redirect = () => {
-        props.history.push("/")
-    }
+    // ログイン状態ならばホームへリダイレクト
+    useEffect(() => {
+        const handleValidatesLogin = () => {
+            if (loggedInStatus) {
+                setTimeout(() => {redirect()}, 3000)
+            }
+        }
+        handleValidatesLogin()
+    })
 
     const handleErrors = () => {
         return (
@@ -41,101 +55,45 @@ function Login(props) {
         )
     }
 
+    const loginRenderer = () => {
+        return (
+            <div className="Login">
+                <h1>ログイン</h1>
+                <div>
+                    {
+                        errors ? handleErrors() : null
+                    }
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        placeholder="email"
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button type="submit">ログイン</button>
+                </form>
+            </div>
+        )
+    }
+
+
     return (
         <div>
-            <h1>ログイン</h1>
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    placeholder="email"
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <input
-                    placeholder="password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-
-                <button type="submit">ログイン</button>
-            </form>
-            <div>
-                {
-                    errors ? handleErrors() : null
-                }
-            </div>
+            {loggedInStatus ?
+                <ErrorMessages {...props} accessErrors="すでにログインしています。" /> :
+                loginRenderer()
+            }
         </div>
     )
 }
 
 export default Login
-
-// import React, { useState } from 'react'
-// import axios from 'axios'
-// import { Link } from 'react-router-dom'
-
-// export default function Login(props) {
-//     console.log(props.history)
-//     const [email, setEmail] = useState("")
-//     const [password, setPassword] = useState("")
-//     const [loginErrors, setLoginErrors] = useState("")
-//     const user =  {
-//         user: {
-//             email: email,
-//             password: password,
-//         }
-//     }
-
-//     const redirect = (props) => {
-//         props.history.push("/")
-//     }
-
-//     const handleSubmit = (event) => {
-//         event.preventDefault()
-//         axios.post("http://localhost:3001/login", user, { withCredentials: true })
-//             .then(response => {
-//                 if (response.data.status === 'created') {
-//                     console.log(props)
-//                     props.handleLogin(response.data)
-//                     redirect(response)
-//                 } else {
-//                     setLoginErrors(response.data.errors)
-//                 }
-//             }).catch(error => {
-//                 console.log("registration error", error)
-//             })
-//     }
-
-//     return (
-//         <div>
-//             <p>ログイン</p>
-//             <div>
-//                 {/* {loginErrors ? props.handleErrors() : null} */}
-//             </div>
-//             <form onSubmit={handleSubmit}>
-//                 <input
-//                     type="email"
-//                     name="email"
-//                     placeholder="メールアドレス"
-//                     value={email}
-//                     onChange={event => setEmail(event.target.value)}
-//                 />
-//                 <input
-//                     type="password"
-//                     name="password"
-//                     placeholder="パスワード"
-//                     value={password}
-//                     onChange={event => setPassword(event.target.value)}
-//                 />
-
-//                 <button type="submit">ログイン</button>
-//             </form>
-//             <Link to="/signup">新規登録はコチラ</Link>
-//             <Link to="/">ホーム</Link>
-//         </div>
-//     )
-// }
