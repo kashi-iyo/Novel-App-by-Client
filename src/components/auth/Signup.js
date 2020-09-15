715469aff98018f4639ea6c88c85990bb87385c2
kasi-iyo <snakeman143@gmail.com>
2020-09-15 22:41:51 +0900

    Signupコンポーネントを編集。 →validateSaveUser、signupValidate、useInput、useAccessAuthLoggingをimport。

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,113 +1,67 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
 
-import ErrorMessages from '../ErrorMessages/ErrorMessages'
+import validateSaveUser from '../CustomHooks/Validate/AuthValidate/validateSaveUser'
+import signupValidate from '../CustomHooks/Validate/AuthValidate/signupValidate'
+import useInput from '../CustomHooks/Auth/useInput'
+import useAccessAuthLogging from '../CustomHooks/Auth/useAccessAuthLogging'
 
 function Signup(props) {
-    const [nickname, setNickname] = useState("")
-    const [accountId, setAccountId] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirmation, setPasswordConfirmation] = useState("")
-    const [errors, setErrors] = useState("")
-    const loggedInStatus = props.loggedInStatus
-    const handleLogin = props.handleLogin
-    const user = {
-        user:
-        {
-            nickname: nickname,
-            account_id: accountId,
-            email: email,
-            password: password,
-            password_confirmation: passwordConfirmation,
-        }
-    }
-
-    // リダイレクト
-    const redirect = () => {
-        props.history.push("/")
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        axios.post('http://localhost:3001/users', user, { withCredentials: true }).then(response => {
-            if (response.data.status === 'created') {
-                handleLogin(response)
-                redirect()
-            } else {
-                setErrors(response.data.errors)
-            }
-        }).catch(error => console.log("エラー: ", error))
-    }
-
-    // ログイン済みの場合ホームへリダイレクト
-    useEffect(() => {
-        const handleValidatesSignup = () => {
-            if (loggedInStatus) {
-                setTimeout(() => {redirect()}, 3000)
-            }
-        }
-        handleValidatesSignup()
+    const { values, handleChange, handleSubmit, errors, saveErrors } = useInput({
+        validate: signupValidate,
+        method: "post",
+        url: 'http://localhost:3001/users',
+        props: props
     })
+    // ログインした状態でこのページへアクセスされた場合ホームへリダイレクトする
+    useAccessAuthLogging(props)
 
-    const handleErrors = () => {
-        return (
-            <div>
-                <ul>
-                    {
-                        errors.map(error => {
-                            return <li key={error}>{error}</li>
-                        })
-                    }
-                </ul>
-            </div>
-        )
-    }
-
+    // 新規登録フォーム
     const signupRenderer = () => {
         return (
             <div>
-                <div>
-                    {
-                        errors ? handleErrors() : null
-                    }
-                </div>
+                {/* Railsからのerrors.full_messagesをレンダリングする */}
+                {validateSaveUser(saveErrors)}
                 <h1>新規登録</h1>
                 <form onSubmit={handleSubmit}>
+                    {errors.nickname && <p className="error">{errors.nickname}</p>}
                     <input
                         placeholder="nickname"
                         type="text"
                         name="nickname"
-                        value={nickname}
-                        onChange={e => setNickname(e.target.value)}
+                        values={values.nickname}
+                        onChange={handleChange}
                     />
+                    {errors.accountId && <p className="error">{errors.accountId}</p>}
                     <input
                         placeholder="account_id"
                         type="text"
                         name="account_id"
-                        value={accountId}
-                        onChange={e => setAccountId(e.target.value)}
+                        values={values.accountId}
+                        onChange={handleChange}
                     />
+                    {errors.email && <p className="error">{errors.email}</p>}
                     <input
                         placeholder="email"
                         type="text"
                         name="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        values={values.email}
+                        onChange={handleChange}
                     />
+                    {errors.password && <p className="error">{errors.password}</p>}
                     <input
                         placeholder="password"
                         type="password"
                         name="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        values={values.password}
+                        onChange={handleChange}
                     />
+                    {errors.passwordConfirmation && <p className="error">{errors.passwordConfirmation}</p>}
                     <input
                         placeholder="password_confirmation"
                         type="password"
                         name="password_confirmation"
-                        value={passwordConfirmation}
-                        onChange={e => setPasswordConfirmation(e.target.value)}
+                        values={values.passwordConfirmation}
+                        onChange={handleChange}
                     />
                     <button type="submit">新規登録</button>
                 </form>
@@ -117,10 +71,7 @@ function Signup(props) {
 
     return (
         <div>
-            {loggedInStatus ?
-                <ErrorMessages {...props} accessErrors="すでに登録済みです。" /> :
-                signupRenderer()
-            }
+            {signupRenderer()}
         </div>
     )
 }
