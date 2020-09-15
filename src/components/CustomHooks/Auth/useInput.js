b3c72fcd94d3bfdcf09ced87f5a8a8677c92712e
kasi-iyo <snakeman143@gmail.com>
2020-09-15 22:42:50 +0900

    useInputカスタムフックを作成。 →フォームから入力された内容をRailsへ送信する役割や、その際に出たエラーの値を保持する役割。

--- src/components/CustomHooks/Auth/useInput.js
+++ src/components/CustomHooks/Auth/useInput.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 import {useState} from 'react'
+import useLoggedIn from './useLoggedIn'
 
+// 認証フォームのカスタムフック
 function useInput({ validate, method, url, props }) {
     const [values, setValues] = useState({})
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState("")
+    const [saveErrors, setSaveErrors] = useState("")
+    const {handleLogin} = useLoggedIn()
 
     const handleChange = e => {
         const { name, value } = e.target
@@ -11,31 +15,30 @@ function useInput({ validate, method, url, props }) {
             ...values,
             [name]: value
         })
-        console.log(values)
-    }
-
-    // リダイレクト
-    const redirect = () => {
-        props.history.push("/")
     }
 
     const handleSubmit = e => {
         e.preventDefault()
+        const redirect = () => {
+            props.history.push("/")
+        }
         setErrors(validate(values))
         axios[method](url,
             { user: values },
-            { widthCredentials: true })
+            { withCredentials: true })
             .then(res => {
                 let data = res.data
-                if (data.logged_in) {
-                    props.handleLogin(data.user)
+                if (data.logged_in || data.status === 'created') {
+                    handleLogin(data.user)
                     redirect()
+                } else if (data.status === 500) {
+                    setSaveErrors(data.errors)
                 }
             })
             .catch(err => console.log(err))
     }
 
-    return {values, handleChange, handleSubmit, errors}
+    return {values, handleChange, handleSubmit, errors, saveErrors}
 }
 
 export default useInput
