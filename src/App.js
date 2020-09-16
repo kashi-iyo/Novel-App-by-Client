1b6a9f9e554b06626fe42c2b6fbe2a4e5a7e2cb7
kasi-iyo <snakeman143@gmail.com>
2020-09-16 20:38:57 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import './App.css'
 import Home from './components/Home/Home'
 import Header from './components/Header/Header'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
-
 import SeriesCreate from './components/Series/SeriesCreate/SeriesCreate'
 import SeriesEdit from './components/Series/SeriesEdit/SeriesEdit'
-
 import NovelsFeed from './components/Novels/NovelsFeed/NovelsFeed'
 import NovelsContents from './components/Novels/NovelsContents/NovelsContents'
-
-import './App.css'
 import useLoggedIn from './components/CustomHooks/Auth/useLoggedIn'
 
 export default function App() {
