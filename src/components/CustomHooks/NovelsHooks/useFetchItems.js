7d8fa592c9952c94438342b283459de33f90f850
kasi-iyo <snakeman143@gmail.com>
2020-09-16 20:33:11 +0900

    useFetchItemsカスタムフック作成。 →シリーズ全件取得。 →シリーズ1件取得。 →1つのシリーズが所有する小説全件取得。 →1つの小説の内容を取得。

---
+++ src/components/CustomHooks/NovelsHooks/useFetchItems.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+
+export default function useItems({method, url}) {
+    const [items, setItems] = useState({})
+    const [errors, setErrors] = useState("")
+
+    useEffect(() => {
+        const getItems = () => {
+            axios[method](url, { withCredentials: true })
+                .then(response => {
+                    let res = response.data
+                    // シリーズ全件取得
+                    if (res.status === 200 && res.keyword === 'index') {
+                        setItems(res.novel_series)
+                    // 1つのシリーズ取得
+                    } else if (res.status === 200 && res.keyword === 'show') {
+                        setItems({ ...res.novel_series, ...res })
+                    // 1つのシリーズが所有する小説全件取得
+                    } else if (res.status === 200 && res.keyword === 'index_of_novels') {
+                        let novel = res.novel_in_series
+                        let novelId = res.novel_id
+                        let seriesTitle = res.series_title
+                        let seriesId = res.series_id
+                        setItems({ ...novel, novelId, seriesTitle, seriesId })
+                    // 非公開時のデータ
+                    } else if (res.keyword === 'unrelease') {
+                        setErrors(res.messages)
+                    }
+                })
+                .catch(error => console.log(error))
+        }
+        getItems()
+    }, [])
+
+    return {
+        items, errors
+    }
+}
