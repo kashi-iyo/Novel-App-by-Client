0ca03c5bb874332f3108bb0e68372713814a91ba
kasi-iyo <snakeman143@gmail.com>
2020-09-17 21:46:12 +0900

    SeriesEditを編集。 →編集用データをuseFetchEditItemsから取得する。

--- src/components/Series/SeriesEdit/SeriesEdit.js
+++ src/components/Series/SeriesEdit/SeriesEdit.js
@@ -1,72 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React from 'react'
+// import { Link } from 'react-router-dom'
 
+import './SeriesEdit'
 import SeriesForm from '../SeriesForm/SeriesForm'
 import ErrorMessages from '../../ErrorMessages/ErrorMessages'
+import useFetchEditItems from '../../CustomHooks/NovelsHooks/useFetchEditItems'
+import useLoggedIn from '../../CustomHooks/AuthHooks/useLoggedIn'
 
+// useEditItemsで取得したデータを、SeriesFormへを渡す
 function SeriesEdit(props) {
-    const [isMounted, setIsMounted] = useState(false)
-    const [seriesTitle, setSeriesTitle] = useState("")
-    const [seriesDescription, setSeriesDescription] = useState("")
-    const [accessErrors, setAccessErrors] = useState("")
-    const loggedInStatus = props.loggedInStatus
+    const url = props.match.url     // Railsから編集データを取得するのに必要なurl
+    const id = props.match.params.id    // SeriesFormへ渡すURLに必要なパラメータ
+    const { loggedInStatus } = useLoggedIn()
+    const { items, errors, setIsMounted } = useFetchEditItems({
+        method: "get",
+        url: `http://localhost:3001/api/v1${url}`,
+        props: props
+    })
 
-    // シリーズのパラメータを含んだパス
-    const path = props.location.pathname
 
-    useEffect(() => {
-        const redirect = () => {
-            props.history.push("/login")
-        }
-        const seriesValue = () => {
-            axios.get(`http://localhost:3001/api/v1/${path}`, { withCredentials: true }).then(response => {
-                console.log(response)
-                if (isMounted && response.data.status === 200) {
-                    setSeriesTitle(response.data.novel_series.series_title)
-                    setSeriesDescription(response.data.novel_series.series_description)
-                } else if (isMounted && response.data.status === 401) {
-                    setTimeout(() => { redirect() }, 3000)
-                    setAccessErrors(response.data.messages)
-                }
-            })
-            .catch(error => console.log('エラー: ', error))
-            }
-        seriesValue()
-        setIsMounted(true)
-
-    }, [path, isMounted, props.history])
-    
-    const handleValidateSeriesEdit = () => {
+    // 編集フォームをレンダリングする
+    const seriesEditForm = () => {
         return (
             <div className='seriesEdit'>
-                <h3>シリーズ編集</h3>
 
                 {/* 小説一覧 */}
-                <Link to="/novels">このシリーズの小説一覧</Link><br></br>
-
-                <SeriesForm {...props}
-                    seriesTitle={seriesTitle}
-                    setSeriesTitle={setSeriesTitle}
-                    seriesDescription={seriesDescription}
-                    setSeriesDescription={setSeriesDescription}
-                    isMountd={isMounted}
-                    setIsMounted={setIsMounted}
-                    path={path}
-                    button="編集を完了する"
-                />
+                {/* <Link to="/novels">このシリーズの小説一覧</Link><br></br> */}
+                {
+                    items ?
+                        <SeriesForm {...props}
+                            novelSeries={items}
+                            setIsMounted={setIsMounted}
+                            method="patch"
+                            url={`http://localhost:3001/api/v1/novel_series/${id}`}
+                            button="編集を完了する"
+                        /> :
+                        null
+                }
             </div>
         )
     }
 
     return (
         <div>
-            {/* ユーザーがログイン&エラーがない場合にtrueを返す */}
-            {loggedInStatus && !accessErrors ?
-                handleValidateSeriesEdit() :
+            {/* ログインし、尚且つエラーの存在しない場合に、編集フォームを表示 */}
+            {loggedInStatus && !errors ?
+                seriesEditForm() :
                 <ErrorMessages {...props}
-                    accessErrors={accessErrors}
-                    loggedInStatus={loggedInStatus}
+                    accessErrors={errors}
                 />
             }
         </div>
