773d3a39e44202672dfddc6db204e5085caddd21
kasi-iyo <snakeman143@gmail.com>
2020-09-17 21:42:01 +0900

    useFetchItemsを微編集。 →繰り返しの記述を定数化。

--- src/components/CustomHooks/NovelsHooks/useFetchItems.js
+++ src/components/CustomHooks/NovelsHooks/useFetchItems.js
@@ -11,21 +11,26 @@ export default function useFetchItems({method, url}) {
             axios[method](url, { withCredentials: true })
                 .then(response => {
                     let res = response.data
+                    let key = res.keyword
+                    let ok = res.status === 200
                     // シリーズ全件取得
-                    if (res.status === 200 && res.keyword === 'index') {
+                    if (ok && key === 'index_of_series') {
                         setItems(res.novel_series)
                     // 1つのシリーズ取得
-                    } else if (res.status === 200 && res.keyword === 'show') {
-                        setItems({ ...res.novel_series, ...res })
+                    } else if (ok && key === 'show_of_series') {
+                        setItems({
+                            ...res.novel_series,
+                            ...res.novel_in_series
+                        })
                     // 1つのシリーズが所有する小説全件取得
-                    } else if (res.status === 200 && res.keyword === 'index_of_novels') {
+                    } else if (ok && key === 'index_of_novels') {
                         let novel = res.novel_in_series
                         let novelId = res.novel_id
                         let seriesTitle = res.series_title
                         let seriesId = res.series_id
                         setItems({ ...novel, novelId, seriesTitle, seriesId })
                     // 非公開時のデータ
-                    } else if (res.keyword === 'unrelease') {
+                    } else if (key === 'unrelease') {
                         setErrors(res.messages)
                     }
                 })
