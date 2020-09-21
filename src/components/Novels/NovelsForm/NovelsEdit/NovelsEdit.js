6ea435e031ed3d7104cc223b75cb4edbe08dcd6e
kasi-iyo <snakeman143@gmail.com>
2020-09-21 20:24:16 +0900

    NovelsEditを作成。 →小説編集用のデータをNovelsFormへ送信する

---
+++ src/components/Novels/NovelsForm/NovelsEdit/NovelsEdit.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import useLoggedIn from '../../../CustomHooks/Auth/useLoggedIn'
+
+import useFetchEditItems from '../../../CustomHooks/NovelsHooks/useFetchEditItems'
+import ErrorMessages from '../../../ErrorMessages/ErrorMessages'
+import NovelsForm from '../NovelsForm'
+
+
+function NovelsEdit(props) {
+    const url = props.match.url
+    const { loggedInStatus, currentUser } = useLoggedIn()
+    const { items, paramId, errors, mounted, setMount } = useFetchEditItems({
+        method: "get",
+        url: `http://localhost:3001/api/v1${url}`,
+        props: props
+    })
+    const seriesId = paramId.seriesId
+    const novelsId = paramId.novelsId
+
+    // 編集フォームをレンダリングする
+    const novelsEditForm = () => {
+        return (
+            <div className='seriesEdit'>
+
+                {/* 小説一覧 */}
+                {/* <Link to="/novels">このシリーズの小説一覧</Link><br></br> */}
+
+                {
+                    items
+                    &&
+                    <NovelsForm {...props}
+                        method="patch"
+                        url={`http://localhost:3001/api/v1//novel_series/${seriesId}/novels/${novelsId}`}
+                        novels={items}
+                        paramId={paramId}
+                        mounted={mounted}
+                        setMount={setMount}
+                        currentUser={currentUser}
+                        formType="edit"
+                        dataType="novel"
+                        button="編集を完了する"
+                    />
+                }
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {/* ログインし、尚且つエラーの存在しない場合に、編集フォームを表示 */}
+            {loggedInStatus && !errors ?
+                novelsEditForm() :
+                <ErrorMessages {...props}
+                    accessErrors={errors}
+                    loggedInStatus={loggedInStatus}
+                />
+            }
+        </div>
+    )
+}
+
+export default NovelsEdit
