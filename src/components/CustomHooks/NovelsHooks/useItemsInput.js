e2654310e8a569686b9caa37dd800b559e23a44d
kasi-iyo <snakeman143@gmail.com>
2020-09-21 19:41:42 +0900

    useItemsInputを編集。 →初期値の定義を、単に作成する場合と編集の場合と更新された場合とで挙動を変更 →編集と作成時とで、Railsへ送信するデータを区別するsendData()メソッドを定義 →シリーズ作成時、シリーズ編集時、小説作成時、小説編集時のデータをRailsから受け取る。

--- src/components/CustomHooks/NovelsHooks/useItemsInput.js
+++ src/components/CustomHooks/NovelsHooks/useItemsInput.js
@@ -1,21 +1,41 @@
 import axios from 'axios'
 import {useState, useEffect} from 'react'
 
-// 投稿系昨日のinputフィールドでの挙動を記述
-// →SeriesFormにて使用
-function useItemsInput({ method, url, mount, sendItems, props }) {
-    // 元からあるデータがあればそれをフォームに表示、入力するたびにその値をフォームに表示
-    const initialState = "" || JSON.parse(localStorage.getItem("keys")) || sendItems
-    const [values, setValues] = useState(initialState)  // 入力データ、作者データなどを格納する
-    const [release, setRelease] = useState(false)   // 公開するかどうか
-    const [successful, setSuccessful] = useState("")    // 成功メッセージ
-    const [errors, setErrors] = useState("")  // エラーメッセージ
+// 投稿系機能のinputフィールドでの挙動を記述
+// →SeriesForm, NovelsFormにて使用
+function useItemsInput({ method, url, mounted, setMount, sendItems, props, formType, dataType }) {
+
+    const [values, setValues] = useState(() => {
+        // 初期値
+        const seriesState = { series_title: "", series_description: "" }
+        const novelState = { novel_title: "", novel_description: "", novel_content: "" }
+        // ローカルストレージから取得
+        const localState = JSON.parse(localStorage.getItem("key"))
+
+        // ローカルストレージに値がない、createフォーム、シリーズデータ
+        if (localState === null && formType === "create" && dataType === "series") {
+            return seriesState
+        // ローカルストレージに値がない、createフォーム、小説データ
+        } else if (localState === null && formType === "create" && dataType === "novel") {
+            return novelState
+        // ローカルストレージに値がない、編集フォーム
+        } else if (localState == null && formType === "edit") {
+            console.log("ok")
+            return sendItems
+        // ローカルストレージに値がある
+        } else if (!!localState) {
+            return localState
+        }
+    })
+    const [release, setRelease] = useState(sendItems ? sendItems.release : false)           // 公開するかどうか
+    const [successful, setSuccessful] = useState("")        // 成功メッセージ
+    const [errors, setErrors] = useState("")                 // エラーメッセージ
     const [existingErrors, setExistingErrors] = useState("")
 
     // 入力データはブラウザに保存
     useEffect(() => {
         localStorage.setItem(
-            "keys", JSON.stringify(values)
+            "key", JSON.stringify(values)
         )
     },[values])
 
@@ -38,40 +58,83 @@ function useItemsInput({ method, url, mount, sendItems, props }) {
     }
 
     // Railsへ送るJSONデータ
-    const items = {
-        novel_series: {
-            series_title: values.series_title,
-            series_description: values.series_description,
-            author: values.author ? values.author : props.user.nickname,
-            release: values.release ? values.release : release
+    const sendData = () => {
+        if (dataType === "series") {
+            return {
+                novel_series: {
+                    series_title: values.series_title,
+                    series_description: values.series_description,
+                    author: props.currentUser,
+                    release: release
+                }
+            }
+        } else if (dataType === "novel") {
+            return {
+                novel: {
+                    novel_title: values.novel_title,
+                    novel_description: values.novel_description,
+                    novel_content: values.novel_content,
+                    release: release
+                }
+            }
         }
     }
+
+
 
     // Railsへデータを送信し、画面を遷移
     const handleSubmit = e => {
         e.preventDefault()
-        const redirect = (params) => {
-            props.history.push(`/novel_series/${params}`)
+        // 作成または編集後に作成された作品のページへリダイレクト
+        const redirect = (seriesId, novelsId) => {
+            let his = props.history
+            if (novelsId !== null) {
+                his.push(`/novel_series/${seriesId}/novels/${novelsId}`)
+            } else if (novelsId === null) {
+                his.push(`/novel_series/${seriesId}`)
+            }
         }
-        // validate内にエラーが発生しなければ処理続行
-        axios[method](url, items, { withCredentials: true })
+        axios[method](url, sendData(), { withCredentials: true })
             .then(response => {
                 let res = response.data
-                if (res.status === 'created') {
+                let created = res.status === 'created'
+                let updated = res.status === 'ok'
+                let seriesId = res.series_id
+                let novelsId = res.novels_id
+                let key = res.keyword
+                if (mounted && created && key === "create_of_series") {
                     setSuccessful(res.successful)
-                    setTimeout(() => {redirect(res.series_id)}, 1500)
-                } else if (res.status === 'ok') {
+                    setTimeout(() => { redirect(seriesId, null) }, 1500)
+                    setErrors("")
+                } else if (mounted && updated && key === "update_of_series") {
                     setSuccessful(res.successful)
-                    setTimeout(() => {redirect(res.series_id)}, 1500)
-                } else if (res.status === 500) {
+                    setTimeout(() => { redirect(seriesId, null) }, 1500)
+                    setErrors("")
+                } else if (mounted && created && key === "create_of_novels") {
+                    setSuccessful(res.successful)
+                    setTimeout(() => { redirect(seriesId, novelsId) }, 1500)
+                } else if (mounted && updated && key === "update_of_novels") {
+                    setSuccessful(res.successful)
+                    setTimeout(() => { redirect(seriesId, novelsId) }, 1500)
+                } else if (mounted && res.status === 401) {
+                    setErrors(res.messages)
+                } else if (mounted && res.status === "unprocessable_entity") {
                     setExistingErrors(res.errors)
                 }
             })
                 .catch(err => setErrors(err))
-        mount(false)
+        setMount(false)
+        localStorage.removeItem("key")
     }
 
-    return {values, release, handleStatusChange, handleChange, handleSubmit, successful, errors, existingErrors}
+    return {
+        values, release,
+        successful, errors,
+        existingErrors,
+        handleStatusChange,
+        handleChange,
+        handleSubmit,
+    }
 }
 
 export default useItemsInput
