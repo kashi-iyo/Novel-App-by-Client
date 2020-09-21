98f057deab3d74e310d9d51496b089771719c799
kasi-iyo <snakeman143@gmail.com>
2020-09-21 19:39:28 +0900

    useFetchItemsを編集。 →小説は[novels, setNovels]を使い、シリーズは[series, setSeries]を使うように変更。

--- src/components/CustomHooks/NovelsHooks/useFetchItems.js
+++ src/components/CustomHooks/NovelsHooks/useFetchItems.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
-
+// Home, Series, NovelsFeed, Novels, NovelsContents にて使用
 export default function useFetchItems({method, url}) {
-    const [items, setItems] = useState({})
+    const [items, setItems] = useState("")
+    const [novels, setNovels] = useState("")
+    const [series, setSeries] = useState("")
     const [errors, setErrors] = useState("")
 
     useEffect(() => {
@@ -15,20 +17,23 @@ export default function useFetchItems({method, url}) {
                     let ok = res.status === 200
                     // シリーズ全件取得
                     if (ok && key === 'index_of_series') {
-                        setItems(res.novel_series)
+                        setItems({ ...res.novel_series })
+                    } else if (ok && key === 'novel_count') {
+                        setItems(res.novel_count)
                     // 1つのシリーズ取得
                     } else if (ok && key === 'show_of_series') {
-                        setItems({
-                            ...res.novel_series,
-                            ...res.novel_in_series
-                        })
+                        console.log(res)
+                        // setItems({ ...res })
+                        setNovels(res.novel_in_series)
+                        setSeries(res.novel_series)
                     // 1つのシリーズが所有する小説全件取得
                     } else if (ok && key === 'index_of_novels') {
                         let novel = res.novel_in_series
                         let novelId = res.novel_id
                         let seriesTitle = res.series_title
                         let seriesId = res.series_id
-                        setItems({ ...novel, novelId, seriesTitle, seriesId })
+                        setSeries({seriesId, seriesTitle})
+                        setNovels({...novel, novelId})
                     // 非公開時のデータ
                     } else if (key === 'unrelease') {
                         setErrors(res.messages)
@@ -37,9 +42,9 @@ export default function useFetchItems({method, url}) {
                 .catch(error => console.log(error))
         }
         getItems()
-    }, [])
+    }, [method, url], items)
 
     return {
-        items, errors
+        items, novels, series, errors
     }
 }
