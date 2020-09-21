b3fdaa6a51a555c4debd1e4b81cf0fa2652d9833
kasi-iyo <snakeman143@gmail.com>
2020-09-21 19:43:07 +0900

    NovelsContentsを編集。 →userをcurrentUserに変更

--- src/components/Novels/NovelsContents/NovelsContents.js
+++ src/components/Novels/NovelsContents/NovelsContents.js
@@ -8,54 +8,53 @@ import useFetchItems from '../../CustomHooks/NovelsHooks/useFetchItems'
 // 小説1話分の内容を表示
 function NovelsContents(props) {
     const url = props.match.url
-    const { user } = useLoggedIn()
-    const { items } = useFetchItems({
+    const { currentUser } = useLoggedIn()
+    const { series, novels } = useFetchItems({
         method: "get",
         url: `http://localhost:3001/api/v1${url}`
     })
-    const author = items.author
-    const seriesId = items.seriesId
-    const seriesTitle = items.seriesTitle
-    const novelTitle = items.novel_title
-    const novelDescription = items.novel_description
-    const novelContent = items.novel_content
-    const editUrl = `/novel_series/${seriesId}/novels/${items.id}/edit`
-    const seriesUrl = `/novel_series/${items.seriesId}`
+    const author = novels.author
+    const editUrl = `/novel_series/${series.seriesId}/novels/${novels.novelId}/edit`
+    const seriesUrl = `/novel_series/${series.seriesId}`
 
     return (
         <div>
             <div className="NovelsContents">
                 {/* シリーズタイトルと作者 */}
                 <div className="NovelsContens__Series">
-                    {
-                        user === author ?
-                            <Link to={editUrl} className="NovelsContents__Edit" >編集する</Link> :
-                            null
-                    }
                     <div className="NovelsContents__SeriesTitle">
                         <p className="seriesTitle">
                             <Link to={seriesUrl}>
-                                {seriesTitle}
+                                {series.seriesTitle}
                             </Link>
                         </p>
                     </div>
-                    <div className="NovelsContents__SeriesWriter">
-                        <span className="writerWrapper">作者名: </span>
-                        <span className="writerName">
-                            <Link>{author}</Link>
-                        </span>
+                    <div className="NovelContents__WriterEditWrapper">
+                        <div className="NovelsContents__SeriesWriter">
+                            <span className="writerWrapper">作者名: </span>
+                            <span className="writerName">
+                                <Link>{author}</Link>
+                            </span>
+                        </div>
+                        {
+                        currentUser === author ?
+                            <Link to={editUrl} className="NovelsContents__Edit" >
+                                編集する
+                            </Link> :
+                            null
+                        }
                     </div>
                 </div>
                 {/* 小説の内容 */}
                 <div className="NovelsContents__Novels">
                     <div className="NovelsContents__NovelsTitle">
-                        {novelTitle}
+                        {novels.novel_title}
                     </div>
                     <div className="NovelsContents__NovelsDescription">
-                        {novelDescription}
+                        {novels.novel_description}
                     </div>
                     <div className="NovelsContents__NovelsContent">
-                        {novelContent}
+                        {novels.novel_content}
                     </div>
                 </div>
                 {/* 小説への評価 */}
