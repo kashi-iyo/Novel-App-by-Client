230d77299589343e73710f806014fbed5378efcf
kasi-iyo <snakeman143@gmail.com>
2020-09-22 21:54:19 +0900

    Spinner.js、SpinnerIcon.jsを作成 →ローディング用スピーナーを作成

---
+++ src/components/CustomHooks/Spinner/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Spinner.css'
+import SpinnerIcon from './SpinnerIcon'
+
+function Spinner(props) {
+    return (
+        <div className="Spinner">
+            <SpinnerIcon />
+        </div>
+    )
+}
+
+export default Spinner
