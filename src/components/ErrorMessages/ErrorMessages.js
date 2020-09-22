3f07b8e64ff4d3f1a3b4f0a902246815e9e4c7eb
kasi-iyo <snakeman143@gmail.com>
2020-09-22 21:57:26 +0900

    ErrorMessages.jsを編集 →コード削ぎ落とし、スタイリング

--- src/components/ErrorMessages/ErrorMessages.js
+++ src/components/ErrorMessages/ErrorMessages.js
@@ -3,45 +3,16 @@ import React from 'react'
 import './ErrorMessages.css'
 
 function ErrorMessages(props) {
-    const releaseErrors = props.releaseErrors
-    const accessErrors = props.accessErrors
+    const errors = props.errors
     const loggedInStatus = props.loggedInStatus
-
-    // 非公開エラー
-    const handleReleaseErrors = () => {
-        return (
-            <div className="Errors">
-                <p className="Errors__release">{releaseErrors}</p>
-                <p>（3秒後に、ホーム画面へリダイレクトされます。）</p>
-            </div>
-        )
-    }
-
-    // アクセスエラー
-    const handleAccessErrors = () => {
-        return (
-            <div className="Errors">
-                <p className="Errors__access">{accessErrors}</p>
-                {loggedInStatus ?
-                    <p>（3秒後に、ホームへリダイレクトされます。）</p> :
-                    <p>（3秒後に、ログインページへリダイレクトされます。）</p>
-                }
-            </div>
-        )
-    }
 
     // アクセスエラーor非公開エラーにより条件分岐
     const errorsRender = () => {
-        if (accessErrors) {
+        if (errors && !loggedInStatus) {
             return (
-                <div>
-                    {handleAccessErrors(accessErrors)}
-                </div>
-            )
-        } else if (releaseErrors) {
-            return (
-                <div>
-                    {handleReleaseErrors(releaseErrors)}
+                <div className="Errors">
+                    <p className="Errors__access">{errors}</p>
+                    <p className="Errors__access">2秒後にページが遷移されます。</p>
                 </div>
             )
         }
