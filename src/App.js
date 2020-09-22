cdb94092003fd1aaabad6de4a5e21f565dcab1c0
kasi-iyo <snakeman143@gmail.com>
2020-09-22 21:54:49 +0900

    App.jsを編集 →isLoadingがtrueの間は、ローディングを表示させる

--- src/App.js
+++ src/App.js
@@ -13,73 +13,76 @@ import NovelsContents from './components/Novels/NovelsContents/NovelsContents'
 import NovelsCreate from './components/Novels/NovelsForm/NovelsCreate/NovelsCreate'
 import NovelsEdit from './components/Novels/NovelsForm/NovelsEdit/NovelsEdit'
 import useLoggedIn from './components/CustomHooks/Auth/useLoggedIn'
-
+import Spinner from './components/CustomHooks/Spinner/Spinner'
 
 export default function App() {
-  const { loggedInStatus } = useLoggedIn()
+  const { currentUser, loggedInStatus, isLoading} = useLoggedIn()
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Route render={props => ( <Header {...props} /> )} />
-        <Switch>
-          <Route exact path={"/"} render={props => (
-              <Home {...props} />
-          )} />
-
-          {/* 認証系機能へのルーティング===================== */}
-          {!loggedInStatus &&
-            <Route exact path={"/login"} render={props => (
-                <Login {...props} />
-            )} />
-          },
-          {!loggedInStatus &&
-            <Route exact path={"/signup"} render={props => (
-              <Signup {...props} />
-            )} />
-          }
-          {/* 認証系機能へのルーティング===================== */}
-
-          {/* 小説系機能へのルーティング =================================== */}
-          <Route
-            exact path="/series_create"
-            render={props => (
-              <SeriesCreate {...props} />
-            )}
-          />
-          <Route
-            exact path={`/novel_series/:id/edit`}
-            render={props => ( <SeriesEdit {...props} /> )}
-          />
-          <Route
-            exact path="/novel_series/:id"
-            render={props => ( <NovelsFeed {...props} /> )}
-          />
+      { !isLoading &&
+        <BrowserRouter>
+          <Route render={props => (<Header {...props} />)} />
           <Switch>
+            <Route exact path={"/"} render={props => (
+              <Home {...props} />
+            )} />
+
+            {/* 認証系機能へのルーティング===================== */}
+            {!loggedInStatus &&
+              <Route exact path={"/login"} render={props => (
+                <Login {...props} />
+              )} />
+            },
+            {!loggedInStatus &&
+              <Route exact path={"/signup"} render={props => (
+                <Signup {...props} />
+              )} />
+            }
+            {/* 認証系機能へのルーティング===================== */}
+
+            {/* 小説系機能へのルーティング =================================== */}
+            <Route
+              exact path="/series_create"
+              render={props => (
+                <SeriesCreate {...props} />
+              )}
+            />
+            <Route
+              exact path={`/novel_series/:id/edit`}
+              render={props => (<SeriesEdit {...props} />)}
+            />
             <Route
               exact path="/novel_series/:id"
-              render={props => ( <NovelsFeed {...props} /> )}
+              render={props => (<NovelsFeed {...props} />)}
             />
-            <Route
-              exact path="/novel_series/:id/novels/:id"
-              render={props => ( <NovelsContents {...props} /> )}
-            />
-            <Route
-              exact path="/novel_series/:id/add_novels"
-              render={props => (
-                <NovelsCreate {...props} />
-              )}
-            />
-            <Route
-              exact path="/novel_series/:id/novels/:id/edit"
-              render={props => (
-                <NovelsEdit {...props} />
-              )}
-            />
+            <Switch>
+              <Route
+                exact path="/novel_series/:id"
+                render={props => (<NovelsFeed {...props} />)}
+              />
+              <Route
+                exact path="/novel_series/:id/novels/:id"
+                render={props => (<NovelsContents {...props} />)}
+              />
+              <Route
+                exact path="/novel_series/:id/add_novels"
+                render={props => (
+                  <NovelsCreate {...props} />
+                )}
+              />
+              <Route
+                exact path="/novel_series/:id/novels/:id/edit"
+                render={props => (
+                  <NovelsEdit {...props} />
+                )}
+              />
+            </Switch>
+            {/* ============================================================== */}
           </Switch>
-          {/* ============================================================== */}
-        </Switch>
       </BrowserRouter>
+      }
+      {isLoading && !currentUser && <Spinner /> }
     </div>
   )
 }
