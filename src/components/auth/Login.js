af0c3aec0b64da09f9d1627fd46ed367b2d062e6
kasi-iyo <snakeman143@gmail.com>
2020-09-22 21:55:34 +0900

    Signup.js、Login.jsを編集 →認証系機能のフォームをスタイリング

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import './auth.css'
 import useInput from '../CustomHooks/Auth/useInput'
 import loginValidate from '../CustomHooks/Validate/AuthValidate/loginValidate'
 import useAccessAuthLogging from '../CustomHooks/Auth/useAccessAuthLogging'
+import ErrorMessages from '../ErrorMessages/ErrorMessages'
+import useLoggedIn from '../CustomHooks/Auth/useLoggedIn'
 
 function Login(props) {
     const { values, handleChange, handleSubmit, errors,}
@@ -12,42 +15,61 @@ function Login(props) {
                 url: 'http://localhost:3001/login',
                 props: props
         })
+    const {loggedInStatus} = useLoggedIn()
     // ログイン状態にこのページへアクセスされたらリダイレクト
     useAccessAuthLogging(props)
 
     // ログインフォーム
     const loginRenderer = () => {
         return (
-            <div className="Login">
-                <h1>ログイン</h1>
-                <form onSubmit={handleSubmit}>
-                    {errors.email && <p>{errors.email}</p>}
-                    <input
-                        placeholder="email"
-                        type="text"
-                        name="email"
-                        values={values.email}
-                        onChange={handleChange}
-                    />
-                    {errors.password && <p>{errors.password}</p>}
-                    <input
-                        placeholder="password"
-                        type="password"
-                        name="password"
-                        values={values.password}
-                        onChange={handleChange}
-                    />
+            <div className="AuthForm">
+                <h1 className="auth__h1">ログイン</h1>
+                <form onSubmit={handleSubmit} className="login__Form">
+                    <div className="email__Wrapper">
+                        {errors.email && <p>{errors.email}</p>}
+                        <label htmlFor="email" className="email__Label">メールアドレス</label>
+                        <input
+                            placeholder="email"
+                            className="email"
+                            type="email"
+                            name="email"
+                            id="email"
+                            values={values.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="password__Wrapper">
+                        {errors.password && <p>{errors.password}</p>}
+                        <label htmlFor="password" className="password__Label">パスワード</label>
+                        <input
+                            placeholder="password"
+                            className="password"
+                            type="password"
+                            name="password"
+                            id="password"
+                            values={values.password}
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                    <button type="submit">ログイン</button>
+                    <button type="submit" className="button auth__btn ">ログイン</button>
                 </form>
             </div>
         )
     }
 
+    const renderer = () => {
+        if (loggedInStatus) {
+            return <ErrorMessages loggedInStatus={loggedInStatus} errors={errors} />
+        } else {
+            return loginRenderer()
+        }
+    }
+
 
     return (
         <div>
-            {loginRenderer()}
+            {renderer()}
         </div>
     )
 }
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import './auth.css'
 import validateSaveUser from '../CustomHooks/Validate/AuthValidate/validateSaveUser'
 import signupValidate from '../CustomHooks/Validate/AuthValidate/signupValidate'
 import useInput from '../CustomHooks/Auth/useInput'
@@ -18,52 +19,77 @@ function Signup(props) {
     // 新規登録フォーム
     const signupRenderer = () => {
         return (
-            <div>
+            <div className="AuthForm">
                 {/* Railsからのerrors.full_messagesをレンダリングする */}
                 {validateSaveUser(saveErrors)}
-                <h1>新規登録</h1>
+                <h1 className="auth__h1">新規登録</h1>
                 <form onSubmit={handleSubmit}>
-                    {errors.nickname && <p className="error">{errors.nickname}</p>}
-                    <input
-                        placeholder="nickname"
-                        type="text"
-                        name="nickname"
-                        values={values.nickname}
-                        onChange={handleChange}
-                    />
-                    {errors.accountId && <p className="error">{errors.accountId}</p>}
-                    <input
-                        placeholder="account_id"
-                        type="text"
-                        name="account_id"
-                        values={values.accountId}
-                        onChange={handleChange}
-                    />
-                    {errors.email && <p className="error">{errors.email}</p>}
-                    <input
-                        placeholder="email"
-                        type="text"
-                        name="email"
-                        values={values.email}
-                        onChange={handleChange}
-                    />
-                    {errors.password && <p className="error">{errors.password}</p>}
-                    <input
-                        placeholder="password"
-                        type="password"
-                        name="password"
-                        values={values.password}
-                        onChange={handleChange}
-                    />
-                    {errors.passwordConfirmation && <p className="error">{errors.passwordConfirmation}</p>}
-                    <input
-                        placeholder="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        values={values.passwordConfirmation}
-                        onChange={handleChange}
-                    />
-                    <button type="submit">新規登録</button>
+                    <div className="nickname__Wrapper">
+                        {errors.nickname && <p className="error">{errors.nickname}</p>}
+                        <label htmlFor="nickname" className="nickname__Label">ユーザー名</label>
+                        <input
+                            placeholder="ユーザー名"
+                            className="nickname"
+                            id="nickname"
+                            type="text"
+                            name="nickname"
+                            values={values.nickname}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="accountId__Wrapper">
+                        {errors.accountId && <p className="error">{errors.accountId}</p>}
+                        <label htmlFor="accountId" className="accountId__Label">ユーザーID</label>
+                        <input
+                            placeholder="ユーザーID"
+                            className="accounrId"
+                            id="accountId"
+                            type="text"
+                            name="account_id"
+                            values={values.accountId}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="email__Wrapper">
+                        {errors.email && <p className="error">{errors.email}</p>}
+                        <label htmlFor="email" className="email__Label">メールアドレス</label>
+                        <input
+                            placeholder="メールアドレス"
+                            className="email"
+                            id="email"
+                            type="email"
+                            name="email"
+                            values={values.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="password__Wrapper">
+                        {errors.password && <p className="error">{errors.password}</p>}
+                        <label htmlFor="password" className="password__Label">パスワード</label>
+                        <input
+                            placeholder="パスワード"
+                            className="password"
+                            id="password"
+                            type="password"
+                            name="password"
+                            values={values.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="passConf__Wrapper">
+                        {errors.passwordConfirmation && <p className="error">{errors.passwordConfirmation}</p>}
+                        <label htmlFor="passCon" className="passConf__Label">パスワード（確認用）</label>
+                        <input
+                            id="passCon"
+                            type="password"
+                            className="passConf"
+                            name="password_confirmation"
+                            placeholder="パスワード（確認用）"
+                            values={values.passwordConfirmation}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit" className="button auth__btn">新規登録</button>
                 </form>
             </div>
         )
