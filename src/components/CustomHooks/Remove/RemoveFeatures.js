69f2c9ce3e0ffbc8d94e9e70704738884822499c
kasi-iyo <snakeman143@gmail.com>
2020-09-24 19:20:11 +0900

    RemoveFeaturesを作成 →削除ボタンとそのモーダルを作成

---
+++ src/components/CustomHooks/Remove/RemoveFeatures.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './RemoveFeatures.css'
+
+function RemoveFeatures({ theme, author, currentUser, handleClick, confirmation, handleOkRemove, handleNoRemove }) {
+    console.log(author, currentUser)
+    return (
+        <div>
+            {/* 削除ボタン */}
+            {author === currentUser &&
+                <div className="removeButton">
+                    <button onClick={handleClick} className="button ">
+                        この{theme}を削除する
+                    </button>
+                </div>
+            }
+            {/* 削除確認もーだる */}
+            <div className="SeriesFeed__RemoveWrapper">
+                {
+                    confirmation &&
+                    <div className="removeModal">
+                        <p className="removeModal__Message">{confirmation}</p>
+                        <input type="button" className="okRemove" onClick={handleOkRemove} value="はい" />
+                        <input type="button" className="noRemove" onClick={handleNoRemove} value="いいえ" />
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default RemoveFeatures
