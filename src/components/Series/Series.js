061df6d68367c23f6fde2bc836e886fac7da2c9a
kasi-iyo <snakeman143@gmail.com>
2020-09-30 23:10:03 +0900

    Series.jsを編集 →シリーズが所有するタグをSeriesTagsコンポーネントへ渡す

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import SeriesTags from './SeriesTags/SeriesTags'
 import './Series.css'
-import SeriesCount from './SeriesCount/SeriesCount'
 
 function Series(props) {
     const seriesId = props.items.id
@@ -13,36 +12,34 @@ function Series(props) {
 
     const handleNovel = () => {
         return (
-            <div className="Series">
-                <div className="Series__top">
-                    <div className="Series__title">
-                        <Link to={`novel_series/${seriesId}`} className="Series__titleLink" >{seriesTitle}</Link>
-                    </div>
-                    <div className="Series__WriterWrapper">
-                        <div className="Series__writer">作者:
-                            <Link className="Series__writerName"> {author}</Link>
+                <div className="Series">
+                    <div className="Series__top">
+                        <div className="Series__title">
+                            <Link to={`novel_series/${seriesId}`} className="Series__titleLink" >{seriesTitle}</Link>
                         </div>
-                        <div className="Series__count">
-                            <SeriesCount {...props} seriesId={seriesId} />
+                        <div className="Series__WriterWrapper">
+                            <div className="Series__writer">作者:
+                                <Link className="Series__writerName"> {author}</Link>
+                            </div>
+                            <div className="Series__count">
+                                （全 {props.items.count} 話）
+                            </div>
                         </div>
                     </div>
+                    <div className="Series__center">
+                        <div className="Series__description">{seriesDescription}</div>
+                    </div>
+                    <div className="Series__bottom">
+                        <div className="Series__reviews">評価数: <Link>5</Link></div>
+                        <div className="Series__favorites">お気に入り数: <Link>5</Link></div>
+                        <div className="Series__comments">コメント数: <Link>5</Link></div>
+                    </div>
+                    <div className="Series__tagWrap">
+                        <ul className="Series__tagUl">
+                            <SeriesTags {...props} seriesId={seriesId} />
+                        </ul>
+                    </div>
                 </div>
-                <div className="Series__center">
-                    <div className="Series__description">{seriesDescription}</div>
-                </div>
-                <div className="Series__bottom">
-                    <div className="Series__reviews">評価数: <Link>5</Link></div>
-                    <div className="Series__favorites">お気に入り数: <Link>5</Link></div>
-                    <div className="Series__comments">コメント数: <Link>5</Link></div>
-                </div>
-                <div className="Series__tagWrap">
-                    <ul className="Series__tagUl">
-                        <li className="Series__tagLi"><Link className="Series__tagLink">タグ</Link></li>
-                        <li className="Series__tagLi"><Link className="Series__tagLink">タグ</Link></li>
-                        <li className="Series__tagLi"><Link className="Series__tagLink">タグ</Link></li>
-                    </ul>
-                </div>
-            </div >
         )
     }
 
