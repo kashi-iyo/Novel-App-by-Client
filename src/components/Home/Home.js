99c1f24e33967b2432837d0c14519cd96f4c16e5
kasi-iyo <snakeman143@gmail.com>
2020-09-30 23:08:50 +0900

    Homeを編集 →タグクラウドを実装 　　→全タグを表示

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,10 @@ import './Home.css'
 import SeriesRanking from './SeriesRanking/SeriesRanking'
 import Series from '../Series/Series'
 import useFetchItems from '../CustomHooks/NovelsHooks/useFetchItems'
+import Tags from './Tags/Tags'
 
-function Home(props) {
-    const { items, isLoading } = useFetchItems({
+function Home() {
+    const { items, count, tags, isLoading } = useFetchItems({
         method: "get",
         url: 'http://localhost:3001'
     })
@@ -14,13 +15,28 @@ function Home(props) {
     return (
         <div className="home">
             {!isLoading &&
-                <div className="home__ranking">
-                    <SeriesRanking>
-                        {Object.keys(items).map(key => (
-                            <Series key={key} items={items[key]} />
-                        ))}
-                    </SeriesRanking>
-                </div>
+                <React.Fragment>
+                    <p className="Caption homeCaption">╋タグクラウド</p>
+                    <div className="TagFeed">
+                        {
+                            tags && Object.keys(tags).map(key => (
+                                <Tags key={key} tags={tags[key]} />
+                            ))
+                        }
+                    </div>
+                    <p className="Caption homeCaption">╋作品一覧（全 {count} 件）</p>
+                    <div className="home__ranking">
+                        <SeriesRanking>
+                        {
+                            Object.keys(items).map(key => (
+                                < Series
+                                    key={key}
+                                    items={items[key]}
+                                />))
+                        }
+                        </SeriesRanking>
+                    </div>
+                </React.Fragment>
             }
         </div>
     )
