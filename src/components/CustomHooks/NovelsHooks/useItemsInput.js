1d3a8bb5e327889a6e6b7bbda1f8537882f5ae4e
kasi-iyo <snakeman143@gmail.com>
2020-10-01 21:48:57 +0900

    useItemsInputを編集 →タグ追加機能用のイベントなどを定義

--- src/components/CustomHooks/NovelsHooks/useItemsInput.js
+++ src/components/CustomHooks/NovelsHooks/useItemsInput.js
@@ -4,7 +4,7 @@ import useRedirect from '../Redirect/useRedirect'
 
 // 投稿系機能のinputフィールドでの挙動を記述
 // →SeriesForm, NovelsFormにて使用
-function useItemsInput({ method, url, mounted, setMount, sendItems, props, formType, dataType }) {
+function useItemsInput({ method, url, sendItems, props, formType, dataType }) {
 
     const [values, setValues] = useState(() => {
         // 初期値
@@ -15,23 +15,36 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
 
         // ローカルストレージに値がある
         if (!!localState) {
-            console.log("localstorage ok")
             return localState
         // ローカルストレージに値がない、createフォーム、シリーズデータ
         }   else if (formType === "create" && dataType === "series") {
             return seriesState
         // ローカルストレージに値がない、createフォーム、小説データ
         } else if (formType === "create" && dataType === "novel") {
-            console.log("create, novel")
             return novelState
         // ローカルストレージに値がない、編集フォーム
         } else if (formType === "edit") {
             return sendItems
         }
     })
+    const [tags, setTags] = useState(() => {
+        const initialTags = []
+        const editTags = []
+        const localTags = JSON.parse(localStorage.getItem("tags"))
+        if (!!localTags) {
+            console.log("localtags ok")
+            return localTags
+        } else if (formType === "create" && dataType === "series") {
+            console.log("initialTags ok")
+            return initialTags
+        } else if (formType === "edit") {
+            return editTags
+        }
+    }) // タグ
     const [release, setRelease] = useState(sendItems ? sendItems.release : false)           // 公開するかどうか
     const [itemSuccess, setItemSuccess] = useState("")        // 成功メッセージ
     const [itemErrors, setItemErrors] = useState("")                 // エラーメッセージ
+    // const [mount, setMount] = useState(false)
     const {redirect} = useRedirect({history: props.history})
 
     // 入力データはブラウザに保存
@@ -39,7 +52,10 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
         localStorage.setItem(
             "key", JSON.stringify(values)
         )
-    },[values])
+        localStorage.setItem(
+            "tags", JSON.stringify(tags)
+        )
+    },[values, tags])
 
     // チェックボックスにチェックを入れた場合にtrueに切り替える。
     const handleStatusChange = () => {
@@ -59,6 +75,26 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
         })
     }
 
+    // サブミットボタンをエンターで発火しないようにする
+    const handleFalse = e => {
+        e.preventDefault()
+    }
+
+    // タグの追加
+    const addTags = event => {
+        const val = event.target.value
+        if (val !== "") {
+            setTags([ ...tags, val ])
+            event.target.value = ""
+        }
+    }
+
+    // タグの削除
+    const removeTags = indexToRemove => {
+        // クリックした要素意外の要素だけを返す（＝クリックした要素を消す）
+        setTags(tags.filter((_, index) => index !== indexToRemove))
+    }
+
     // Railsへ送るJSONデータ
     const sendData = () => {
         if (dataType === "series") {
@@ -67,7 +103,8 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
                     series_title: values.series_title,
                     series_description: values.series_description,
                     author: props.currentUser,
-                    release: release
+                    release: release,
+                    novel_tag_name: tags.join()
                 }
             }
         } else if (dataType === "novel") {
@@ -76,7 +113,8 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
                     novel_title: values.novel_title,
                     novel_description: values.novel_description,
                     novel_content: values.novel_content,
-                    release: release
+                    release: release,
+                    novel_tag_name: tags.join()
                 }
             }
         }
@@ -87,6 +125,7 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
     const handleSubmit = e => {
         e.preventDefault()
         localStorage.removeItem("key")
+        localStorage.removeItem("tags")
         axios[method](url, sendData(), { withCredentials: true })
             .then(response => {
                 let res = response.data
@@ -95,33 +134,35 @@ function useItemsInput({ method, url, mounted, setMount, sendItems, props, formT
                 let seriesId = res.series_id
                 let novelsId = res.novels_id
                 let key = res.keyword
-                if (mounted && created && key === "create_of_series") {
+                if (created && key === "create_of_series") {
                     setItemSuccess(res.successful)
                     setTimeout(() => { redirect(`/novel_series/${seriesId}`) }, 1500)
-                } else if (mounted && updated && key === "update_of_series") {
+                } else if (updated && key === "update_of_series") {
                     setItemSuccess(res.successful)
                     setTimeout(() => { redirect(`/novel_series/${seriesId}`) }, 1500)
-                } else if (mounted && created && key === "create_of_novels") {
+                } else if (created && key === "create_of_novels") {
                     setItemSuccess(res.successful)
                     setTimeout(() => { redirect(`/novel_series/${seriesId}/novels/${novelsId}`) }, 1500)
-                } else if (mounted && updated && key === "update_of_novels") {
+                } else if (updated && key === "update_of_novels") {
                     setItemSuccess(res.successful)
                     setTimeout(() => { redirect(`/novel_series/${seriesId}/novels/${novelsId}`) }, 1500)
-                } else if (mounted && res.status === 401) {
+                } else if (res.status === 401) {
                     setItemErrors(res.messages)
-                } else if (mounted && res.status === "unprocessable_entity") {
+                } else if (res.status === "unprocessable_entity") {
                     setItemErrors(res.errors)
                 }
             })
-                .catch(err => setItemErrors(err))
-        setMount(false)
+            .catch(err => setItemErrors(err))
         setItemSuccess("")
-        setTimeout(() => setItemErrors(""), 3000 )
+        setTimeout(() => setItemErrors(""), 3000)
     }
 
     return {
-        values, release,
+        values,
+        tags, addTags, removeTags,
+        release,
         itemSuccess, itemErrors,
+        handleFalse,
         handleStatusChange,
         handleChange,
         handleSubmit,
