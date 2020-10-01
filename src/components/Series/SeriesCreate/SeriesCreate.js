4ad98847998e0448e783a499cef87526feb5a380
kasi-iyo <snakeman143@gmail.com>
2020-10-01 21:50:20 +0900

    SeriesCreateを編集 →マウントを削除

--- src/components/Series/SeriesCreate/SeriesCreate.js
+++ src/components/Series/SeriesCreate/SeriesCreate.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 import SeriesForm from '../SeriesForm/SeriesForm'
 import ErrorMessages from '../../ErrorMessages/ErrorMessages'
@@ -6,7 +6,6 @@ import ErrorMessages from '../../ErrorMessages/ErrorMessages'
 // シリーズ作成フォームをレンダリングする。
 // SeriesFormへデータを渡す。
 function SeriesCreate(props) {
-    const [mounted, setMount] = useState(true)
 
     // SeriesFormへデータを渡す
     const seriesCreateForm = () => {
@@ -14,8 +13,6 @@ function SeriesCreate(props) {
             <div className="seriesCreate">
                 <SeriesForm {...props}
                     method="post"
-                    mounted={mounted}
-                    setMount={setMount}
                     url={`http://localhost:3001/api/v1/novel_series`}
                     currentUser={props.currentUser}
                     formType="create"
@@ -27,8 +24,10 @@ function SeriesCreate(props) {
     }
 
     const renderer = () => {
+    console.log(props.loggedInStatus)
+        // ユーザーがログインしているかどうかを確認
         if (props.loggedInStatus) {
-            seriesCreateForm()
+            return seriesCreateForm()
         } else {
             return <ErrorMessages {...props} errors={"アクセス権限がありません。"} />
         }
@@ -36,7 +35,7 @@ function SeriesCreate(props) {
 
     return (
         <div>
-            {/* ユーザーがログインしているかどうかを確認*/}
+            
             {renderer()}
         </div>
     )
