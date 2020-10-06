6f485d4fc02b6560a1d99a3916cdbfec2d023c27
kasi-iyo <snakeman143@gmail.com>
2020-10-06 21:36:36 +0900

    Homeを編集 →isLoadingがtrueの間はローダーを表示させる →Seriesコンポーネントに、シリーズデータを渡す

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,45 +4,44 @@ import './Home.css'
 import SeriesRanking from './SeriesRanking/SeriesRanking'
 import Series from '../Series/Series'
 import useFetchItems from '../CustomHooks/NovelsHooks/useFetchItems'
-import OneTags from '../CustomHooks/Tags/OneTags/OneTags'
+import Spinner from '../CustomHooks/Spinner/Spinner'
+
 
 function Home() {
-    const { items, count, tags, isLoading } = useFetchItems({
+    const { items, count, isLoading } = useFetchItems({
         method: "get",
         url: 'http://localhost:3001'
     })
 
     return (
-        <div className="home">
-            {!isLoading &&
-                <React.Fragment>
-                    <p className="Caption homeCaption">╋タグクラウド</p>
-                    <div className="TagFeed">
-                        {
-                            tags && Object.keys(tags).map(key => {
-                                let id = tags[key].id
-                                let tag = tags[key].novel_tag_name
-                                return (
-                                    <OneTags key={key} link={`/search_series_by_tag/${id}`} tag={tag}  />
-                                )
-                            })
-                        }
-                    </div>
-                    <p className="Caption homeCaption">╋作品一覧（全 {count} 件）</p>
+        <React.Fragment>
+            {isLoading ? <Spinner /> :
+                <div className="home">
+                    <p className="Caption">╋作品一覧（全 {count} 件）</p>
                     <div className="home__ranking">
                         <SeriesRanking>
                         {
-                            Object.keys(items).map(key => (
-                                < Series
-                                    key={key}
-                                    items={items[key]}
-                                />))
+                            Object.keys(items).map(key => {
+                                let id = items[key].id
+                                let title = items[key].series_title
+                                let description = items[key].series_description
+                                let author = items[key].author
+                                let release = items[key].release
+                                let count = items[key].count
+                                let userId = items[key].user_id
+                                return (
+                                    <Series
+                                        key={key} userId={userId}
+                                        id={id} title={title} description={description}
+                                        author={author} release={release} count={count}
+                                    />)
+                            })
                         }
                         </SeriesRanking>
                     </div>
-                </React.Fragment>
+                </div>
             }
-        </div>
+        </React.Fragment>
     )
 }
 
