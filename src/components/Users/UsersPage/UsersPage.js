b0518f12eaa6f0a9bc3f5cb2c9515c9ee98f7ae0
kasi-iyo <snakeman143@gmail.com>
2020-10-09 20:42:23 +0900

    UsersPageを編集 →ページネーションを実装

--- src/components/Users/UsersPage/UsersPage.js
+++ src/components/Users/UsersPage/UsersPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './UsersPage.css'
 import useFetchUserItems from '../../CustomHooks/UsersHooks/useFetchUserItems'
@@ -8,8 +8,8 @@ import UsersPageTop from './UsersPageTop/UsersPageTop'
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
-import UsersFavoriteSeries from '../UsersFavoriteSeries/UsersFavoriteSeries'
 import Spinner from '../../CustomHooks/Spinner/Spinner'
+import UsersPagination from '../../Pagination/UsersPagination'
 
 // ユーザーページ
 function UsersPage(props) {
@@ -20,6 +20,15 @@ function UsersPage(props) {
             editUrl: `http://localhost:3001/users/${userId}/edit`,
             props: props,
     })
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage] = useState(10)
+
+
+    // ページネーション用の投稿データ
+    const indexOfLastPost = currentPage * postsPerPage  // 1x10
+    const indexOfFirstPost = indexOfLastPost - postsPerPage // 10 - 10
+    const currentSeries = usersSeries.slice(indexOfFirstPost, indexOfLastPost) // slie(0, 10)
+    const currentFavoriteSeries = favoriteSeries.slice(indexOfFirstPost, indexOfLastPost)
 
     return (
         <React.Fragment>
@@ -39,28 +48,60 @@ function UsersPage(props) {
                                 {/* 投稿作品とお気に入りのシリーズタイトル */}
                                 <div className="UsersPage__PostedSeriesWrapper">
                                     <TabList>
-                                        <Tab><p className="Tab">投稿作品{seriesCount && <span>（{seriesCount}）</span>}</p></Tab>
-                                        <Tab><p className="Tab">お気に入り作品{favoriteSeriesCount && <span>（{favoriteSeriesCount}）</span>}</p></Tab>
+                                        <Tab>
+                                            <p className="Tab">
+                                                投稿作品{seriesCount && <span>（{seriesCount}）</span>}
+                                            </p>
+                                        </Tab>
+                                        <Tab>
+                                            <p className="Tab">
+                                                お気に入り作品{favoriteSeriesCount && <span>（{favoriteSeriesCount}）</span>}
+                                            </p>
+                                        </Tab>
                                     </TabList>
                                     {/* 投稿作品たち */}
                                     <TabPanel>
+                                        <UsersPagination
+                                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                            totalPosts={usersSeries.length} // 記事数
+                                            currentPage={currentPage}
+                                            setCurrentPage={setCurrentPage}
+                                        />
                                         <ul>
                                             {
-                                                Object.keys(usersSeries).map(key => (
-                                                    <UsersSeries key={key} series={usersSeries[key]} />
+                                                Object.keys(currentSeries).map(key => (
+                                                    <UsersSeries key={key} series={currentSeries[key]} />
                                                 ))
                                             }
                                         </ul>
+                                        <UsersPagination
+                                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                            totalPosts={usersSeries.length} // 記事数
+                                            currentPage={currentPage}
+                                            setCurrentPage={setCurrentPage}
+                                        />
                                     </TabPanel>
                                     {/* お気に入り作品達 */}
                                     <TabPanel>
+                                        <UsersPagination
+                                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                            totalPosts={favoriteSeries.length} // 記事数
+                                            currentPage={currentPage}
+                                            setCurrentPage={setCurrentPage}
+                                        />
                                         <ul className="UsersPage__UsersFavoritesUl">
                                             {favoriteSeries &&
-                                                Object.keys(favoriteSeries).map(key => (
-                                                    <UsersFavoriteSeries key={key} favoriteSeries={favoriteSeries[key]} />
+                                                Object.keys(currentFavoriteSeries).map(key => (
+                                                    <UsersSeries key={key} series={currentFavoriteSeries[key]} />
                                                 ))
                                             }
                                         </ul>
+                                        <UsersPagination
+                                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                            totalPosts={favoriteSeries.length} // 記事数
+                                            currentPage={currentPage}
+                                            setCurrentPage={setCurrentPage}
+                                        />
                                     </TabPanel>
                                 </div>
                             </Tabs>
