9fabe022d58397b6c81096fc7f7ecc57b57e071a
kasi-iyo <snakeman143@gmail.com>
2020-10-11 01:17:03 +0900

    FavoritesButtonを編集 →少しのリファクタリング

--- src/components/Novels/NovelsContents/Favorites/FavoritesButton.js
+++ src/components/Novels/NovelsContents/Favorites/FavoritesButton.js
@@ -4,11 +4,12 @@ import './FavoritesButton.css'
 import useFavorites from '../../../CustomHooks/Favorites/useFavorites'
 import favoritesIcon from '../../../../img/favorites.png'
 
-function FavoritesButton(props) {
+function FavoritesButton({ props, userId, novelId, currentUser }) {
     const { favorite, favoriteCount, errors, handleFavorites, handleUnFavorites } = useFavorites({
         props: props,
-        novelId: props.match.params.novel_id,
-        userId: props.userId
+        novelId: novelId,
+        userId: userId,
+        currentUser: currentUser
     })
 
     return (
@@ -18,19 +19,17 @@ function FavoritesButton(props) {
                 {/* お気に入りされているかどうかで表示切り替え */}
                 {!favorite ?
                     <button type="submit" className="FavoritesButton" onClick={handleFavorites} >
-                        <div className="FavoritesWrapper" >
                             <FavoriteIcon fontSize="default" color="action" className="FavoritesIcon" />
                             <span className="FavoritesSpan">お気に入りする</span>
-                        </div>
                     </button> :
                     <button type="submit" className="FavoritesButton" onClick={handleUnFavorites} >
-                        <div className="FavoritesWrapper" >
                             <img src={favoritesIcon} alt="favorites" className="FavoritedIcon "/>
                             <span className="FavoritedSpan">お気に入り済</span>
-                        </div>
                     </button>
                 }
-                <p className="FavoritesCount">{favoriteCount}</p>
+                <p className="FavoritesCount">
+                    <span>{favoriteCount}</span>
+                </p>
             </div>
         </React.Fragment>
     )
