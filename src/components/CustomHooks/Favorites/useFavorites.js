b237ff7cae43c56855767e97200a8fb2223681bd
kasi-iyo <snakeman143@gmail.com>
2020-10-11 23:44:59 +0900

    お気に入り機能の編集 →お気に入り数の取得方法を、useFavoritesカスタムフックからuseFetchItemsへ変更

--- src/components/CustomHooks/Favorites/useFavorites.js
+++ src/components/CustomHooks/Favorites/useFavorites.js
@@ -3,19 +3,23 @@ import { useEffect, useState } from 'react'
 
 function useFavorites({ novelId, userId, currentUser }) {
     const [favorite, setFavorite] = useState(false)
-    const [favoriteCount, setFavoriteCount] = useState("")
+    const [update, setUpdate] = useState("initial")
+    // const [favoriteCount, setFavoriteCount] = useState("")
+    const [updateFavoritesCount, setUpdateFavoritesCount] = useState("")
     const [favoriter, setFavoriter] = useState([])
     const [errors, setErrors] = useState("")
-console.log(favoriter)
+
     // マウント時にお気に入りのステータスをチェックする
     useEffect(() => {
-        axios.get(`http://localhost:3001/api/v1/novel_favorites/${novelId}`, { withCredentials: true })
+        let mount = true
+        const getFavorites = () => {
+            axios.get(`http://localhost:3001/api/v1/novel_favorites/${novelId}`, { withCredentials: true })
             .then(response => {
                 let res = response.data
-                if (res.status === 200) {
-                    setFavoriteCount(res.favorites_count)
+                if (mount && res.status === 200) {
+                    setUpdateFavoritesCount(res.favorites_count)
                     setFavoriter(res.favorites)
-                    res.favorites.map(favorite => {
+                    res.favorites.forEach(favorite => {
                         let userId2 = parseInt(favorite.user_id)
                         if (userId === userId2) {
                             setFavorite(true)
@@ -23,7 +27,10 @@ console.log(favoriter)
                     })
                 }
             }).catch(err => console.log(err))
-    },[setFavorite])
+        }
+        getFavorites()
+        return () => { mount = false }
+    },[update, novelId, userId])
 
     // お気に入り時の挙動
     const handleFavorites = () => {
@@ -41,7 +48,7 @@ console.log(favoriter)
                 let res = response.data
                 if (res.status === 'created') {
                     setFavorite(true)
-                    setFavoriteCount(res.favorites_count)
+                    setUpdate("on")
                 } else if (res.status === "unprocessable_entity") {
                     setErrors(res.errors)
                 } else if (res.status === 401) {
@@ -57,13 +64,13 @@ console.log(favoriter)
             .then(response => {
                 if (response.data.head === 'no_content') {
                     setFavorite(false)
-                    setFavoriteCount(response.data.favorites_count)
+                    setUpdate("off")
                 }
             })
             .catch(err => console.log(err))
     }
 
-    return {favorite, favoriteCount, favoriter, errors, handleFavorites, handleUnFavorites}
+    return {favorite, updateFavoritesCount, favoriter, errors, handleFavorites, handleUnFavorites}
 }
 
 export default useFavorites
--- src/components/Novels/NovelsContents/Favorites/FavoritesButton.js
+++ src/components/Novels/NovelsContents/Favorites/FavoritesButton.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder'
 import './FavoritesButton.css'
 import useFavorites from '../../../CustomHooks/Favorites/useFavorites'
 import favoritesIcon from '../../../../img/favorites.png'
+import FavoritesUsersWrapper from './FavoritesUsersWrapper'
 
-function FavoritesButton({ props, userId, novelId, currentUser }) {
-    const { favorite, favoriteCount, errors, handleFavorites, handleUnFavorites } = useFavorites({
+function FavoritesButton({ props, userId, novelId, currentUser, favoritesCount }) {
+    const [on, setOn] = useState(false)
+    const { favorite, updateFavoritesCount, favoriter, errors, handleFavorites, handleUnFavorites } = useFavorites({
         props: props,
         novelId: novelId,
         userId: userId,
         currentUser: currentUser
     })
+
+    // お気に入りユーザーを開く
+    const handleOn = () => {
+        if (on) {
+            setOn(false)
+        } else if (!on) {
+            setOn(true)
+        }
+    }
 
     return (
         <React.Fragment>
@@ -27,9 +38,19 @@ function FavoritesButton({ props, userId, novelId, currentUser }) {
                             <span className="FavoritedSpan">お気に入り済</span>
                     </button>
                 }
-                <p className="FavoritesCount">
-                    <span>{favoriteCount}</span>
+                {/* お気に入り数の表示切り替え */}
+                <p className="FavoritesCount" onClick={() => handleOn()}>
+                    {updateFavoritesCount ?
+                        <span>{updateFavoritesCount}</span> :
+                        <span>{favoritesCount}</span>
+                    }
                 </p>
+                {/* クリックによりユーザーを表示 */}
+                {on &&
+                    <ul className="Favorites__Ul">
+                        <FavoritesUsersWrapper favoriter={favoriter} />
+                    </ul>
+                }
             </div>
         </React.Fragment>
     )
