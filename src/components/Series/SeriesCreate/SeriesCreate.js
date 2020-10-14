90c6498fcf9b6425caa7d4f833e5797a6cfd5569
kasi-iyo <snakeman143@gmail.com>
2020-10-15 00:07:46 +0900

    no message

--- src/components/Series/SeriesCreate/SeriesCreate.js
+++ src/components/Series/SeriesCreate/SeriesCreate.js
@@ -1,39 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import SeriesForm from '../SeriesForm/SeriesForm'
 import ErrorMessages from '../../ErrorMessages/ErrorMessages'
+import useFetchItems from '../../CustomHooks/NovelsHooks/useFetchItems'
+import Spinner from '../../Spinner/Spinner'
 
 // シリーズ作成フォームをレンダリングする。
 // SeriesFormへデータを渡す。
-function SeriesCreate(props) {
+function SeriesCreate({ loggedInStatus, currentUser, history, isLoading }) {
+    const [mount, setMount] = useState(true)
 
     // SeriesFormへデータを渡す
     const seriesCreateForm = () => {
         return (
-            <div className="seriesCreate">
-                <SeriesForm {...props}
-                    method="post"
-                    url={`http://localhost:3001/api/v1/novel_series`}
-                    formType="create"
-                    dataType="series"
-                    button="作成する"
-                />
-            </div>
+            <React.Fragment>
+                <div className="seriesCreate">
+                    <SeriesForm
+                        method="post"
+                        url={`http://localhost:3001/api/v1/novel_series`}
+                        formType="create"
+                        dataType="series"
+                        button="作成する"
+                        history={history}
+                        currentUser={currentUser}
+                        setMount={setMount}
+                    />
+                </div>
+            </React.Fragment>
         )
     }
 
     const renderer = () => {
         // ユーザーがログインしているかどうかを確認
-        if (props.loggedInStatus) {
+        if (!!mount && loggedInStatus) {
             return seriesCreateForm()
-        } else if (!props.loggedInStatus) {
+        } else if (!!mount && !loggedInStatus) {
             return <ErrorMessages errors={"アクセス権限がありません。"} />
         }
     }
 
     return (
         <div>
-            {renderer()}
+            {isLoading ? <Spinner /> : renderer()}
         </div>
     )
 }
