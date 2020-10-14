85ba2315998f6ec55309eb356d313e53837111c0
kasi-iyo <snakeman143@gmail.com>
2020-10-15 00:07:33 +0900

    no message

--- src/components/Novels/NovelsInNovelsFeed/NovelsInNovelsFeed.js
+++ src/components/Novels/NovelsInNovelsFeed/NovelsInNovelsFeed.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import Novels from '../Novels'
 import './NovelsInFeed.css'
 
-function NovelsInNovelsFeed(props) {
+// Novels.jsに投稿データを渡す
+function NovelsInNovelsFeed({novel, userId}) {
     return (
         <div className="NovelsInFeed">
-            {props.children}
+            {
+                novel ?
+                    novel.map(novel => (
+                        <Novels
+                            key={novel.id}
+                            novelId={novel.id}
+                            novelTitle={novel.novel_title}
+                            release={novel.release}
+                            seriesId={novel.novel_series_id}
+                            novelUserId={novel.user_id}
+                            userId={userId}
+                        />
+                    )) : null
+            }
         </div>
     )
 }
