3a0ee55f2a204b7584b649d18838d684260e770d
kasi-iyo <snakeman143@gmail.com>
2020-10-17 01:51:34 +0900

    useFetchItems → useFetchTagsからデータ取得するようにする ページネーションの機能を加える

--- src/components/Tags/TagHasSeries/TagHasSeries.js
+++ src/components/Tags/TagHasSeries/TagHasSeries.js
@@ -1,40 +1,46 @@
 import React from 'react'
-
-import useFetchItems from '../../CustomHooks/NovelsHooks/useFetchItems'
-import Series from '../../Series/Series'
-import SeriesRanking from '../../Series/SeriesWrapper/SeriesWrapper'
+import usePagination from '../../../CustomHooks/Pagination/usePagination'
+import useFetchTags from '../../../CustomHooks/Tags/useFetchTags'
+import Pagination from '../../Pagination/Pagination'
+import SeriesWrapper from '../../Series/SeriesWrapper/SeriesWrapper'
 import './TagHasSeries.css'
 
 // タグに関連付けられたシリーズの表示
-function TagHasSeries(props) {
-    let id = String(props.match.params.id)
-    const { items, count, tags } = useFetchItems({
+function TagHasSeries({ tagId, pageNo }) {
+    // タグに関連づけされたデータを取得
+    const { items, count, tags } = useFetchTags({
         method: "get",
-        url: `http://localhost:3001/api/v1/series_in_tag/${id}`
+        url: `http://localhost:3001/api/v1/novel_tags/${tagId}`
+    })
+
+    // ページネーション用のデータ
+    const { postsPerPage, currentPage, currentItems } = usePagination({
+        pageNo: pageNo,
+        items: items
     })
 
     return (
         <div className="TagsSeries">
+            <Pagination
+                postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                totalPosts={items.length} // 記事数
+                currentPage={currentPage}
+                paginateHref={`/search_series_by_tag/${tagId}/page/`}
+            />
             <p className="Caption TagsSeries__caption">╋ {tags.novel_tag_name} の作品一覧（全 {count} 件）</p>
-            <div className="home__ranking">
-                <SeriesRanking>
-                    {
-                        Object.keys(items).map(key => {
-                            return (
-                                <Series
-                                    key={items[key].id}
-                                    id={items[key].id}
-                                    count={items[key].count}
-                                    author={items[key].author}
-                                    release={items[key].release}
-                                    userId={items[key].user_id}
-                                    title={items[key].series_title}
-                                    description={items[key].series_description}
-                                />)
-                        })
-                    }
-                </SeriesRanking>
+            <div className="homeWrapper">
+                {
+                    Object.keys(currentItems).map(key => (
+                    <SeriesWrapper key={key} items={currentItems[key]} />
+                    ))
+                }
             </div>
+            <Pagination
+                postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                totalPosts={items.length} // 記事数
+                currentPage={currentPage}
+                paginateHref={`/search_series_by_tag/${tagId}/page/`}
+            />
         </div>
     )
 }
