4dbbd4e5644f9fe3e1b64f10caec47f1d853b38c
kasi-iyo <snakeman143@gmail.com>
2020-10-17 01:44:47 +0900

    ページネーション機能をusePaginetionカスタムフックとして抽出

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,24 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import './Home.css'
 import SeriesWrapper from '../Series/SeriesWrapper/SeriesWrapper'
-import useFetchItems from '../CustomHooks/NovelsHooks/useFetchItems'
+import useFetchItems from '../../CustomHooks/NovelsHooks/useFetchItems'
 import Spinner from '../Spinner/Spinner'
 import Pagination from '../Pagination/Pagination'
+import usePagination from '../../CustomHooks/Pagination/usePagination'
 
 // ホームページ
 function Home({ seriesNo }) {
-    const [currentPage] = useState(seriesNo)
-    const [postsPerPage] = useState(5)
     const { items, count, isLoading } = useFetchItems({
         method: "get",
         url: 'http://localhost:3001',
     })
 
-    // ページネーション用の投稿データ
-    const indexOfLastPost = currentPage * postsPerPage
-    const indexOfFirstPost = indexOfLastPost - postsPerPage
-    const currentPosts = items.slice(indexOfFirstPost, indexOfLastPost)
-
+    // ページネーション用のデータ
+    const { postsPerPage, currentPage, currentItems } = usePagination({
+        pageNo: seriesNo,
+        items: items
+    })
     return (
         <React.Fragment>
             {isLoading ? <Spinner /> :
@@ -27,21 +26,21 @@ function Home({ seriesNo }) {
                     <Pagination
                         postsPerPage={postsPerPage}  //1Pに表示する記事の数
                         totalPosts={items.length} // 記事数
-                        seriesNo={currentPage}
+                        currentPage={currentPage}
                         paginateHref={`/Series/`}
                     />
                     <p className="Caption">╋作品一覧（全 {count} 件）</p>
                     <div className="homeWrapper">
-                        {
-                            Object.keys(currentPosts).map(key => (
-                            <SeriesWrapper key={key} items={currentPosts[key]} />
+                        {currentItems &&
+                            currentItems.map(post => (
+                            <SeriesWrapper key={post.id} items={post} />
                             ))
                         }
                     </div>
                     <Pagination
                         postsPerPage={postsPerPage}  //1Pに表示する記事の数
                         totalPosts={items.length} // 記事数
-                        seriesNo={currentPage}
+                        currentPage={currentPage}
                         paginateHref={`/Series/`}
                     />
                 </div>
