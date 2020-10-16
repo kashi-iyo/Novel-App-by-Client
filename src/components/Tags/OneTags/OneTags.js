092d25302eff7b5212e49bfc0a68996125fd9596
kasi-iyo <snakeman143@gmail.com>
2020-10-17 01:47:13 +0900

    count: タグが持つシリーズの数

--- src/components/Tags/OneTags/OneTags.js
+++ src/components/Tags/OneTags/OneTags.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import './OneTags.css'
 
 // 1つのタグを表示させる
-function OneTags({tagName, link}) {
+function OneTags({tagName, link, count}) {
     return (
         <Link to={link} className="UsersPage__tagLink">
             <li className="UsersPage__tagLi" >
-                {tagName}
+                {tagName} <span>（{count}）</span>
             </li>
         </Link>
     )
