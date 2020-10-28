df6a4d7d34df77ec74d6d6a0d878c3bf35186d3f
kasi-iyo <snakeman143@gmail.com>
2020-10-28 23:34:24 +0900

    親コンポーネントからステートを変更するためのメソッドをpropsとして受け取り、子コンポーネントへ渡す

--- src/components/Comment/OneCommentWrapper.js
+++ src/components/Comment/OneCommentWrapper.js
@@ -2,24 +2,12 @@ import React from 'react'
 import OneComment from './OneComment'
 
 // OneCommentへデータを渡すためのコンポーネント
-function OneCommentWrapper({ userId, commentItems }) {
-    const arr = commentItems.commentsData
+function OneCommentWrapper({ userId, commentsItems, setCommentsItems, setSuccess}) {
+    const arr = commentsItems.commentsUser
 
     return (
         <React.Fragment>
-            {/* {commentItems.commentsData.map(comment => (
-                <OneComment
-                    key={comment.comment_id}
-                    userId={userId}
-                    commentId={comment.comment_id}
-                    commentNovelId={comment.comment_novel_id}
-                    commentUserId={comment.comment_user_id}
-                    content={comment.content}
-                    commentUser={comment.commenter}
-                    commentItems={commentItems}
-                />
-            ))} */}
-            {
+            {arr ?
                 Object.keys(arr).map(key => {
                     let a = arr[key]
                     return (
@@ -32,10 +20,13 @@ function OneCommentWrapper({ userId, commentItems }) {
                             commentUserId={a.comment_user_id}
                             content={a.content}
                             commentUser={a.commenter}
-                            commentItems={commentItems}
+                            commentsItems={commentsItems}
+                            setCommentsItems={setCommentsItems}
+                            setSuccess={setSuccess}
                         />
                     )
-                })
+                }) :
+                <p>コメントはありません。</p>
             }
         </React.Fragment>
     )
