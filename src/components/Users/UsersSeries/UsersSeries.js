14134a2454f46a0f50d2ab78f4afd6a3dbe04793
kasi-iyo <snakeman143@gmail.com>
2020-11-01 01:06:52 +0900

    no message

--- src/components/Users/UsersSeries/UsersSeries.js
+++ src/components/Users/UsersSeries/UsersSeries.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import Series from '../../Series/Series'
-import './UsersSeries.css'
+// import React from 'react'
+// import Series from '../../Series/Series'
+// import './UsersSeries.css'
 
-// ユーザーが投稿したシリーズ
-function UsersSeries({series}) {
+// // ユーザーが投稿したシリーズ
+// function UsersSeries({series}) {
 
-    return (
-        <div className="UsersSeries" >
-            {series.length === 0 ? null :
-                <Series
-                    seriesId={series.series.id}
-                    author={series.series.author}
-                    commentsCount={series.comments_count}
-                    favoritesCount={series.favorites_count}
-                    release={series.series.release}
-                    description={series.series.series_description}
-                    title={series.series.series_title}
-                    novelsCount={series.novels_count}
-                    tags={series.tags}
-                    userId={series.series.user_id}
-                />
-            }
-        </div>
-    )
-}
+//     return (
+//         <div className="UsersSeries" >
+//             {series.length === 0 ? null :
+//                 <Series
+//                     seriesId={series.series.id}
+//                     author={series.series.author}
+//                     commentsCount={series.comments_count}
+//                     favoritesCount={series.favorites_count}
+//                     release={series.series.release}
+//                     description={series.series.series_description}
+//                     title={series.series.series_title}
+//                     novelsCount={series.novels_count}
+//                     tags={series.tags}
+//                     userId={series.series.user_id}
+//                 />
+//             }
+//         </div>
+//     )
+// }
 
-export default UsersSeries
+// export default UsersSeries
