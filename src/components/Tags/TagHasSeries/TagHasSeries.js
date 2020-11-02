6869ccb07353b0a984ce52a55e55400ebd197e7e
kasi-iyo <snakeman143@gmail.com>
2020-11-02 23:59:50 +0900

    データの表示をDisplayMultipleItemsで行わせる

--- src/components/Tags/TagHasSeries/TagHasSeries.js
+++ src/components/Tags/TagHasSeries/TagHasSeries.js
@@ -1,45 +1,28 @@
 import React from 'react'
-import usePagination from '../../../CustomHooks/Pagination/usePagination'
+
 import useFetchTags from '../../../CustomHooks/Tags/useFetchTags'
-import Pagination from '../../Pagination/Pagination'
-import SeriesWrapper from '../../Series/SeriesWrapper/SeriesWrapper'
+import DisplayMultipleItems from '../../DisplayMultipleItems/DisplayMultipleItems'
 import './TagHasSeries.css'
 
 // タグに関連付けられたシリーズの表示
-function TagHasSeries({ tagId, pageNo }) {
+function TagHasSeries({ tagId, pageNumber }) {
     // タグに関連づけされたデータを取得
-    const { items, tags } = useFetchTags({
+    const { items, tags, isLoading } = useFetchTags({
         method: "get",
         url: `http://localhost:3001/api/v1/novel_tags/${tagId}`
     })
 
-    // ページネーション用のデータ
-    const { postsPerPage, currentPage, currentItems, indexOfLastPost, indexOfFirstPost } = usePagination({
-        pageNo: pageNo,
-        items: items
-    })
-
     return (
-        <div className="TagsSeries">
-            <p className="TagHasUsers__Count">{tags.has_data_count} 件の作品が登録しています。 （ {indexOfFirstPost + 1} - {items.length < 5 ? items.length : indexOfLastPost}件 ）</p>
-            <p className="Caption TagsSeries__caption">╋ {tags.tag_name} を登録している作品</p>
-            <Pagination
-                postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                totalPosts={items.length} // 記事数
-                currentPage={currentPage}
+        <React.Fragment>
+            <DisplayMultipleItems
+                items={items}
+                caption={`╋ ${tags.tag_name} を登録している作品 `}
+                record={`${tags.has_data_count} 件の作品が登録しています。`}
+                pageNumber={pageNumber}
+                isLoading={isLoading}
                 paginateHref={`/search_series_by_tag/${tagId}/page/`}
             />
-            {/* シリーズ一覧 */}
-            <div className="homeWrapper">
-                <SeriesWrapper items={currentItems} />
-            </div>
-            <Pagination
-                postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                totalPosts={items.length} // 記事数
-                currentPage={currentPage}
-                paginateHref={`/search_series_by_tag/${tagId}/page/`}
-            />
-        </div>
+        </React.Fragment>
     )
 }
 
