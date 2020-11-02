b1d2d0095e5c1dad22ebd801799bd691f6ed4905
kasi-iyo <snakeman143@gmail.com>
2020-11-03 00:13:07 +0900

    no message

--- src/CustomHooks/Pagination/usePagination.js
+++ src/CustomHooks/Pagination/usePagination.js
@@ -1,20 +1,18 @@
 import {useState} from 'react'
 
-function usePagination({pageNo, perPage, items, items2}) {
-    const [currentPage, setCurrentPage] = useState(pageNo)
-    const [postsPerPage] = useState(perPage ? perPage : 5)
+function usePagination({pageNumber, perPage, items }) {
+    const [currentPage, setCurrentPage] = useState(pageNumber)
+    const [postsPerPage] = useState(perPage ? perPage : 7)
 
     // ページネーション用の投稿データ
     // 1つのページに表示させたい記事の最後の記事のインデックス
     const indexOfLastPost = currentPage * postsPerPage
     // 1つのページに表示させたい記事の最初の記事のインデックス
     const indexOfFirstPost = indexOfLastPost - postsPerPage
-    // ユーザーのページでは同時にページネーションしたいデータを取得するので2つの変数を用意
-    const currentItems = items && items.slice(indexOfFirstPost, indexOfLastPost)
-    const currentItems2 = items2 && items2.slice(indexOfFirstPost, indexOfLastPost)
+    const currentItems = items.slice(indexOfFirstPost, indexOfLastPost)
 
     return {
-        postsPerPage, setCurrentPage, currentPage, currentItems, currentItems2, indexOfLastPost, indexOfFirstPost
+        postsPerPage, setCurrentPage, currentPage, currentItems, indexOfLastPost, indexOfFirstPost
     }
 }
 
