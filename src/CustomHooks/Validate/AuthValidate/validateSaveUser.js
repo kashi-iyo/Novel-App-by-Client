c6ba33c8a37c4f6c72a07625c62b29ababf3d62b
kasi-iyo <snakeman143@gmail.com>
2020-11-03 23:07:28 +0900

    no message

--- src/CustomHooks/Validate/AuthValidate/validateSaveUser.js
+++ src/CustomHooks/Validate/AuthValidate/validateSaveUser.js
@@ -2,23 +2,16 @@ import React from 'react'
 
 // Railsからのerrors.full_messagesをレンダリングする。
 export default function validateSaveUser(errors) {
-    const handleSaveErrors = () => {
-        return (
-            <div>
-                <ul>
-                    {
-                        errors.map(error => {
-                            return <li className="error" key={error}>{error}</li>
-                        })
-                    }
-                </ul>
-            </div>
-        )
-    }
 
     return (
-        <div>
-            {errors? handleSaveErrors() : null}
-        </div>
+        <React.Fragment>
+            <ul>
+                {
+                    errors.map(error => {
+                        return <li className="error" key={error}>{error}</li>
+                    })
+                }
+            </ul>
+        </React.Fragment>
     )
 }
