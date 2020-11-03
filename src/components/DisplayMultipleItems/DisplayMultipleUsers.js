ec4258aa67831d19784e9e22b1c30dc75cc734a1
kasi-iyo <snakeman143@gmail.com>
2020-11-03 23:02:57 +0900

    DisplayMultipleUsers.js作成 ユーザー一覧を表示する

---
+++ src/components/DisplayMultipleItems/DisplayMultipleUsers.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import usePagination from '../../CustomHooks/Pagination/usePagination'
+import Pagination from '../Pagination/Pagination'
+import Spinner from '../Spinner/Spinner'
+import UsersWrapper from '../Users/UsersWrapper'
+import './DisplayMultipleUsers.css'
+
+
+// ユーザー一覧表示
+// → RelationshipUsers/TagHasUsers
+function DisplayMultipleUsers({pageNumber, perPage, users, isLoading,  recordCaption, paginateHref, caption}) {
+
+    // ページネーション用のデータ
+    const { postsPerPage, currentPage, currentItems, indexOfLastPost, indexOfFirstPost} = usePagination({
+        pageNumber: pageNumber,
+        perPage: perPage,
+        items: users
+    })
+
+    return (
+        <React.Fragment>
+            {/* ローダー */}
+            {isLoading ? <Spinner /> :
+                <div className="display-multiple-users">
+                    {/* 見出し */}
+                    <h2 className="display-multiple-items--caption">
+                        {caption}
+                    </h2>
+                    {/* 件数見出し */}
+                    <p className="display-multiple-items--record-numbers">
+                        {recordCaption}
+                        （ {indexOfFirstPost + 1} - {users.length < postsPerPage ? users.length : indexOfLastPost}件 ）
+                    </p>
+                    {/* ページネーション */}
+                    <Pagination
+                        postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                        totalPosts={users.length} // 記事数
+                        currentPage={currentPage}
+                        paginateHref={paginateHref}
+                    />
+                    {/* ユーザー一覧 */}
+                    <UsersWrapper items={currentItems} />
+                    {/* ページネーション */}
+                    <Pagination
+                        postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                        totalPosts={users.length} // 記事数
+                        currentPage={currentPage}
+                        paginateHref={paginateHref}
+                    />
+                </div>
+            }
+        </React.Fragment>
+    )
+}
+
+export default DisplayMultipleUsers
