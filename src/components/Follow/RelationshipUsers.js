35a1f161340dd6e8c3f714bb0a56a3f68ef1fa52
kasi-iyo <snakeman143@gmail.com>
2020-11-03 23:03:21 +0900

    ユーザーを表示するためのデータをDisplayMultipleUsersへ渡す

--- src/components/Follow/RelationshipUsers.js
+++ src/components/Follow/RelationshipUsers.js
@@ -1,52 +1,29 @@
 import React from 'react'
 import useFollow from '../../CustomHooks/FollowHooks/useFollow'
-import usePagination from '../../CustomHooks/Pagination/usePagination'
-import Pagination from '../Pagination/Pagination'
-import Spinner from '../Spinner/Spinner'
-import UsersWrapper from '../Users/UsersWrapper'
+import DisplayMultipleUsers from '../DisplayMultipleItems/DisplayMultipleUsers'
 
-function RelationshipUsers({ userId, pageNumber, dataType }) {
+// フォローユーザー一覧/フォロワー一覧の表示
+function RelationshipUsers({ userId, relationshipsParams, pageNumber }) {
     const {items, isLoading} =  useFollow({
         method: "get",
-        url: `http://localhost:3001/api/v1/relationships/${userId}/${dataType}`,
+        url: `http://localhost:3001/api/v1/relationships/${userId}/${relationshipsParams}`,
     })
     const users = items.users
     const usersCount = items.users_count
 
-    // ページネーション用のデータ
-    const { postsPerPage, currentPage, currentItems, indexOfLastPost, indexOfFirstPost} = usePagination({
-        pageNumber: pageNumber,
-        perPage: 30,
-        items: users
-    })
-
     return (
         <React.Fragment>
-            {isLoading ? <Spinner /> :
-                <div className="TagHasUsers">
-                    <p className="TagHasUsers__Count">
-                        {usersCount} 人のユーザー
-                        （ {indexOfFirstPost + 1} - {users.length < 30 ? users.length : indexOfLastPost}件 ）
-                    </p>
-                    <h2 className="Caption UsersCaption">
-                        ╋ {items.user} さんの{dataType === "followings" ? "フォローユーザー" : "フォロワー"}
-                    </h2>
-                    <Pagination
-                        postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                        totalPosts={users.length} // 記事数
-                        currentPage={currentPage}
-                        paginateHref={`/users/${userId}/${dataType}/`}
-                    />
-                    {/* ユーザー一覧 */}
-                    <UsersWrapper items={currentItems} />
-                    <Pagination
-                        postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                        totalPosts={users.length} // 記事数
-                        currentPage={currentPage}
-                        paginateHref={`/users/${userId}/${dataType}/`}
-                    />
-                </div>
-            }
+            <DisplayMultipleUsers
+                isLoading={isLoading}
+                users={users}
+                caption={`${items.user}さんの${relationshipsParams === "followings" ? "フォローユーザー" : "フォロワー"}`}
+                recordCaption={`${usersCount} 人のユーザー`}
+                usersCount={usersCount}
+                perPage={30}
+                pageNumber={pageNumber}
+                paginateHref={`/users/${userId}/${relationshipsParams}/`}
+                relationshipsParams={relationshipsParams}
+            />
         </React.Fragment>
     )
 }
