b53b6a6a58134882e7d952a879e3b049d47f4f96
kasi-iyo <snakeman143@gmail.com>
2020-11-04 22:57:59 +0900

    no message

--- src/components/DisplayMultipleItems/DisplayMultipleItems.js
+++ src/components/DisplayMultipleItems/DisplayMultipleItems.js
@@ -20,7 +20,7 @@ function DisplayMultipleItems({ items, pageNumber, caption, record, isLoading, p
     return (
         <React.Fragment>
             {isLoading ? <Spinner /> :
-                <div className="display-multiple-items">
+                < div className="display-multiple-items">
                     {caption && <h2 className="display-multiple-items--caption">{caption}</h2>}
                     <div className="display-multiple-items--record-wrapper">
                         <p className="display-multiple-items--record-numbers">{record}（ {indexOfFirstPost + 1} - {items.length < postsPerPage ? items.length : indexOfLastPost}件 ）</p>
@@ -31,14 +31,14 @@ function DisplayMultipleItems({ items, pageNumber, caption, record, isLoading, p
                             selectingValue={selectingValue}
                             selectingParams={selectingParams}
                             menuItems={[
-                            { id: 1, value: "新着順", select_params: "new" },
-                            { id: 2, value: "投稿が古い順", select_params: "old" },
-                            { id: 3, value: "お気に入りが多い順", select_params: "more_favo" },
-                            { id: 4, value: "お気に入りが少ない順", select_params: "less_favo" },
-                            { id: 5, value: "コメントが多い順", select_params: "more_comment" },
-                            { id: 6, value: "コメントが少ない順", select_params: "less_comment" },
+                                { id: 1, value: "新着順", select_params: "new" },
+                                { id: 2, value: "投稿が古い順", select_params: "old" },
+                                { id: 3, value: "お気に入りが多い順", select_params: "more_favo" },
+                                { id: 4, value: "お気に入りが少ない順", select_params: "less_favo" },
+                                { id: 5, value: "コメントが多い順", select_params: "more_comment" },
+                                { id: 6, value: "コメントが少ない順", select_params: "less_comment" },
                             ]}
-                            />}
+                        />}
                     </div>
                     {userSeries ?
                         <UsersPagination
