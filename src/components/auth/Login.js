2bd4e729a435e9f8496298fc8dc0ee1b3f94044a
kasi-iyo <snakeman143@gmail.com>
2020-11-04 22:56:38 +0900

    history：ログイン後にプロフィールページへリダイレクトするために使う dataType：新規登録フォームと区別するのに使う handleLogin：ログインさせる handleMessages：フラッシュメッセージをログイン後にセットするのに使う

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,21 +5,24 @@ import useInput from '../../CustomHooks/Auth/useInput'
 import loginValidate from '../../CustomHooks/Validate/AuthValidate/loginValidate'
 import validateSaveUser from '../../CustomHooks/Validate/AuthValidate/validateSaveUser'
 
-function Login({history}) {
+function Login({history, handleLogin, handleMessages}) {
     const { values, handleChange, handleSubmit, errors, saveErrors}
         = useInput({
-                validate: loginValidate,
-                method: "post",
-                url: 'http://localhost:3001/login',
-                history: history
+            validate: loginValidate,
+            method: "post",
+            url: 'http://localhost:3001/login',
+            history: history,   // リダイレクト用
+            dataType: "login",  // 送信フォームをSignup.jsと区別
+            handleLogin: handleLogin,   // ログインを行わせる
+            handleMessages: handleMessages  // フラッシュメッセージの表示に使用
         })
 
     return (
         <div className="auth-form">
             {saveErrors && validateSaveUser(saveErrors)}
-            <h1 className="auth-form--h1">ログイン</h1>
+            <h1 className="auth-form--h1">ログインフォーム</h1>
             <form onSubmit={handleSubmit}>
-            <div className="auth-form--email_wrapper">
+            <div className="auth-form--email-wrapper">
                 {errors.email && <p>{errors.email}</p>}
                 <label htmlFor="email">メールアドレス</label>
                 <input
