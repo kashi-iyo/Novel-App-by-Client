718976b2a68179c03c38c299e75faa7d79cc4c00
kasi-iyo <snakeman143@gmail.com>
2020-11-06 22:48:12 +0900

    ErrorMessagesコンポーネントを編集 エラーメッセージのタイプにより、表示させるエラーメッセージを変える

--- src/components/ErrorMessages/ErrorMessages.js
+++ src/components/ErrorMessages/ErrorMessages.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './ErrorMessages.css'
 
 // アクセスエラーor非公開エラーにより条件分岐
-function ErrorMessages({errors}) {
-
+function ErrorMessages({errors, errorType}) {
     return (
-        <div>
-            <div className="Errors">
-                <p className="Errors__access">{errors}</p>
+        <React.Fragment>
+            {errorType === "errors" && <div>
+                <div className="Errors">
+                    <p className="Errors__access">{errors}</p>
+                </div>
+            </div>}
+            {errorType === "series-create" &&
+            <div className="error-messages--series-create-wrapper">
+                <p className="error-messages--series-create">
+                    作品を投稿するには、ログインまたは新規登録が必要です。
+                </p>
+                <Link to={`/login`}>
+                    ログインはコチラから
+                </Link>
+                <Link to={`/signup`}>
+                    新規登録はコチラから
+                </Link>
             </div>
-        </div>
+            }
+        </React.Fragment>
     )
 }
 
