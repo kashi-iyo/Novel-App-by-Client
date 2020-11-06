77271201527ae94b2bec488444ed5ce6ea6d9a84
kasi-iyo <snakeman143@gmail.com>
2020-11-06 22:52:07 +0900

    handleLogin(), handleLogout()の処理の中でフラッシュメッセージの表示処理を行うようにしたので、書き換え

--- src/CustomHooks/Auth/useInput.js
+++ src/CustomHooks/Auth/useInput.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {useState} from 'react'
 
 // 認証フォームのカスタムフック
-function useInput({ validate, method, url, history, dataType, handleLogin, handleLogout, handleMessages }) {
+function useInput({ validate, method, url, history, dataType, handleLogin, handleLogout, handleFlashMessages }) {
     const [values, setValues] = useState(() => {
         // Signup, Loginによって初期値を切り替え
         const signupValues = {
@@ -44,25 +44,24 @@ function useInput({ validate, method, url, history, dataType, handleLogin, handl
                 .then(response => {
                     console.log(response)
                     let res = response.data
+                    let status = res.status
                     let obj = res.object
                     // 新規登録成功
-                    if (res.status === "created") {
-                        handleLogin(obj)
-                        handleMessages(res.successful)
+                    if (status === "created") {
+                        handleLogin({ user: obj, success: res.successful })
                         redirect(`/users/${obj.id}`)
                     }
                     // ログイン成功
-                    if (res.logged_in) {
-                        handleLogin(obj)
-                        handleMessages(res.successful)
+                    if (status === 200 && res.logged_in) {
+                        handleLogin({ user: obj, success: res.successful })
                         redirect(`/users/${obj.id}`)
-                        // 新規登録失敗
-                    } else if (res.status === "unprocessable_entity") {
+                    // 新規登録失敗
+                    } else if (status === "unprocessable_entity") {
                         setSaveErrors(res.errors)
-                        // 入力内容によるログイン失敗
-                        // ログインしているのにログイン
-                        // 新規登録しているのに新規登録しようとする場合
-                    } else if (res.status === "unauthorized") {
+                    // 入力内容によるログイン失敗
+                    // ログインしているのにログイン
+                    // 新規登録しているのに新規登録しようとする場合
+                    } else if (status === "unauthorized") {
                         setErrors(res.errors)
                     }
                 })
@@ -78,13 +77,15 @@ function useInput({ validate, method, url, history, dataType, handleLogin, handl
         axios.delete('http://localhost:3001/logout',
             { withCredentials: true })
             .then(response => {
+                console.log("logoutClick: クリック")
                 let res = response.data
-                if (res.status === 200 && res.logged_out) {
-                    handleLogout()
-                    handleMessages(res.successful)
+                if (res.status === 200 && !res.logged_in) {
+                    console.log("logout: 成功")
+                    handleLogout({success: res.successful})
                     redirect()
                 } else if (res.status === "unauthorized") {
-                    setErrors(res.errors)
+                    console.log("logout: 失敗")
+                    handleFlashMessages({ errors: res.errors })
                 }
             })
             .catch(error => console.log(error))
@@ -92,24 +93,26 @@ function useInput({ validate, method, url, history, dataType, handleLogin, handl
 
     // 採用担当者様専用ログインフォーム
     const handleLoginForRecruit = () => {
-        console.log("handleLoginForRecruit: ok")
         const redirect = (url) => {
             history.push(url)
         }
         axios.post(`http://localhost:3001/login`,
             {
                 user: {
-                    email: "login_for_recruit@recruit.com",
+                    email: "recruit@recruit.com",
                     password: "123456"
             }},
             { withCredentials: true })
             .then(response => {
+                console.log("handleLoginForRecruit: ok")
                 let res = response.data
                 let obj = res.object
                 if (res.logged_in) {
-                    handleLogin(obj)
-                    handleMessages(res.successful)
+                    console.log("response: ", res)
+                    handleLogin({ user: obj, success: res.successful })
                     redirect(`/users/${obj.id}`)
+                } else if (res.status === "unauthorized") {
+                    handleFlashMessages({ errors: res.errors })
                 }
             })
             .catch(err => console.log(err))
