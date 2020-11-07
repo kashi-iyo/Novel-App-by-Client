ef24852a8721b64c36a323eb372927196a34fb24
kasi-iyo <snakeman143@gmail.com>
2020-11-08 00:20:41 +0900

    no message

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,10 +3,9 @@ import React from 'react'
 import './auth.css'
 import useInput from '../../CustomHooks/Auth/useInput'
 import loginValidate from '../../CustomHooks/Validate/AuthValidate/loginValidate'
-import validateSaveUser from '../../CustomHooks/Validate/AuthValidate/validateSaveUser'
 
 function Login({history, handleLogin, handleMessages}) {
-    const { values, handleChange, handleSubmit, errors, saveErrors}
+    const { values, handleChange, handleSubmit, errors,unauthorizedErrors}
         = useInput({
             validate: loginValidate,
             method: "post",
@@ -19,36 +18,35 @@ function Login({history, handleLogin, handleMessages}) {
 
     return (
         <div className="auth-form">
-            {saveErrors && validateSaveUser(saveErrors)}
             <h1 className="auth-form--h1">ログインフォーム</h1>
             <form onSubmit={handleSubmit}>
-            <div className="auth-form--email-wrapper">
-                {errors.email && <p>{errors.email}</p>}
-                <label htmlFor="email">メールアドレス</label>
-                <input
-                    placeholder="email"
-                    type="email"
-                    name="email"
-                    id="email"
-                    values={values.email}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="auth-form--password-wrapper">
-                {errors.password && <p>{errors.password}</p>}
-                <label htmlFor="password">パスワード</label>
-                <input
-                    placeholder="password"
-                    type="password"
-                    name="password"
-                    id="password"
-                    values={values.password}
-                    onChange={handleChange}
-                />
-            </div>
-
-            <button type="submit" className="button">ログイン</button>
-        </form>
+                <div className="auth-form--email-wrapper">
+                    {errors.email && <p>{errors.email}</p>}
+                    <label htmlFor="email">メールアドレス</label>
+                    <input
+                        placeholder="email"
+                        type="email"
+                        name="email"
+                        id="email"
+                        values={values.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="auth-form--password-wrapper">
+                    {errors.password && <p>{errors.password}</p>}
+                    <label htmlFor="password">パスワード</label>
+                    <input
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        id="password"
+                        values={values.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit" className="button">ログイン</button>
+            </form>
+            {unauthorizedErrors && <p className="error">{unauthorizedErrors}</p>}
         </div>
     )
 }
