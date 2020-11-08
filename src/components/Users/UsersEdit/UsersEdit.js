4bda5c205b58fe64d2586a9a6b1eefe84181bd06
kasi-iyo <snakeman143@gmail.com>
2020-11-08 23:09:51 +0900

    文字数バリデーション用コンポーネントをimport ボタンをコンポーネント化してそれを使用

--- src/components/Users/UsersEdit/UsersEdit.js
+++ src/components/Users/UsersEdit/UsersEdit.js
@@ -2,47 +2,59 @@ import React from 'react'
 import classNames from 'classnames'
 import './UsersEdit.css'
 import useFetchUserItems from '../../../CustomHooks/UsersHooks/useFetchUserItems'
-import Flash from '../../Flash/Flash'
 import InputTag from '../../Series/SeriesTagForm/SeriesTagForm'
+import ValidateWordsCounts from '../../ValidateWordsCounts/ValidateWordsCounts'
+import Button from '../../Button/Button'
 
 // ユーザー編集フォーム
-function UsersEdit({userId, history}) {
+function UsersEdit({userId, history, handleFlashMessages}) {
     const { editUsers, usersTags, addTags, removeTags, handleFalse, handleChange, handleSubmit, success, errors } = useFetchUserItems({
         method: "get",
         url: `http://localhost:3001/api/v1/users/${userId}/edit`,
         updateMethod: "patch",
         updateUrl: `http://localhost:3001/api/v1/users/${userId}`,
         userId: userId,
-        history: history
+        history: history,
+        handleFlashMessages: handleFlashMessages
     })
     const limitErrors = usersTags.length > 5 ? true : false
-    const buttonClass = classNames("button", { "noButton":usersTags.length > 5 })
+    const nickname = editUsers && editUsers.nickname
+    const profile = editUsers && editUsers.profile
 
     return (
         <div>
-            <Flash Success={success} Errors={errors} />
             <div className="users-edit--form-wrapper">
                 <h2 className="caption">プロフィール編集</h2>
                 {/* Enterでの送信阻止 */}
                 <form onSubmit={e => handleFalse(e)} className="users-edit--form" >
                     <div className="users-edit--nickname-wrapper">
                         <label htmlFor="nickname">ニックネーム</label>
+                        <ValidateWordsCounts
+                            wordsLength={nickname.length}
+                            upperLimit={30}
+                            isRequire={true}
+                        />
                         <input
                             type="text"
                             id="nickname"
                             name="nickname"
                             className="editNickname"
-                            value={editUsers.nickname}
+                            value={nickname}
                             onChange={handleChange}
                         />
                     </div>
                     <div className="users-edit--profile-wrapper">
                         <label htmlFor="profile">紹介文</label>
+                        <ValidateWordsCounts
+                            wordsLength={profile.length}
+                            upperLimit={200}
+                            isRequire={false}
+                        />
                         <textarea
                             id="profile"
                             className="editProfile"
                             name="profile"
-                            value={editUsers.profile}
+                            value={profile}
                             onChange={handleChange}
                         />
                     </div>
@@ -56,7 +68,18 @@ function UsersEdit({userId, history}) {
                             limitErrors={limitErrors}
                         />
                     </div>
-                    <button type="button" onClick={handleSubmit} className={buttonClass}>変更を保存する</button>
+                    <Button
+                        handleSubmit={handleSubmit}
+                        badCase={
+                            nickname.length === 0 ||
+                            nickname.length > 30 ||
+                            profile.length > 200 ||
+                            usersTags.length > 5 ?
+                            true :
+                            false
+                        }
+                        buttonValue="変更を保存する"
+                    />
                 </form>
             </div>
         </div>
