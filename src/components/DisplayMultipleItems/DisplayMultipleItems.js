7ead5dc97c64eb66de5c274b0c7b9bfb687b1087
kasi-iyo <snakeman143@gmail.com>
2020-11-09 19:09:57 +0900

    配列データが存在しない場合にはキャプション以外何も表示させない

--- src/components/DisplayMultipleItems/DisplayMultipleItems.js
+++ src/components/DisplayMultipleItems/DisplayMultipleItems.js
@@ -22,54 +22,62 @@ function DisplayMultipleItems({ items, pageNumber, caption, record, isLoading, p
             {isLoading ? <Spinner /> :
                 < div className="display-multiple-items">
                     {caption && <h2 className="display-multiple-items--caption">{caption}</h2>}
-                    <div className="display-multiple-items--record-wrapper">
-                        <p className="display-multiple-items--record-numbers">{record}（ {indexOfFirstPost + 1} - {items.length < postsPerPage ? items.length : indexOfLastPost}件 ）</p>
-                        {/* ドロップダウンBOXユーザーページでは表示しない */}
-                        {!userSeries && <SelectBox
-                            dataType={dataType}
-                            history={history}
-                            selectingValue={selectingValue}
-                            selectingParams={selectingParams}
-                            menuItems={[
-                                { id: 1, value: "新着順", select_params: "new" },
-                                { id: 2, value: "投稿が古い順", select_params: "old" },
-                                { id: 3, value: "お気に入りが多い順", select_params: "more_favo" },
-                                { id: 4, value: "お気に入りが少ない順", select_params: "less_favo" },
-                                { id: 5, value: "コメントが多い順", select_params: "more_comment" },
-                                { id: 6, value: "コメントが少ない順", select_params: "less_comment" },
-                            ]}
-                        />}
-                    </div>
-                    {userSeries ?
-                        <UsersPagination
-                            postsPerPage={postsPerPage}
-                            totalPosts={items.length}
-                            currentPage={currentPage}
-                            setCurrentPage={setCurrentPage}
-                        /> :
-                        <Pagination
-                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                            totalPosts={items.length} // 記事数
-                            currentPage={currentPage}
-                            paginateHref={paginateHref}
-                        />
-                    }
-                    <div className="display-multiple-items--items-wrapper">
-                        <SeriesWrapper items={currentItems} userId={userId} />
-                    </div>
-                    {userSeries ?
-                        <UsersPagination
-                            postsPerPage={postsPerPage}
-                            totalPosts={items.length}
-                            currentPage={currentPage}
-                            setCurrentPage={setCurrentPage}
-                        /> :
-                        <Pagination
-                            postsPerPage={postsPerPage}  //1Pに表示する記事の数
-                            totalPosts={items.length} // 記事数
-                            currentPage={currentPage}
-                            paginateHref={paginateHref}
-                        />
+                    {items.length !== 0 &&
+                        <React.Fragment>
+                            <div className="display-multiple-items--record-wrapper">
+                                <p className="display-multiple-items--record-numbers">{record}（ {indexOfFirstPost + 1} - {items.length < postsPerPage ? items.length : indexOfLastPost}件 ）</p>
+                                {/* ドロップダウンBOXユーザーページでは表示しない */}
+                                {!userSeries && <SelectBox
+                                    dataType={dataType}
+                                    history={history}
+                                    selectingValue={selectingValue}
+                                    selectingParams={selectingParams}
+                                    menuItems={[
+                                        { id: 1, value: "新着順", select_params: "new" },
+                                        { id: 2, value: "投稿が古い順", select_params: "old" },
+                                        { id: 3, value: "お気に入りが多い順", select_params: "more_favo" },
+                                        { id: 4, value: "お気に入りが少ない順", select_params: "less_favo" },
+                                        { id: 5, value: "コメントが多い順", select_params: "more_comment" },
+                                        { id: 6, value: "コメントが少ない順", select_params: "less_comment" },
+                                    ]}
+                                />}
+                            </div>
+                            {
+                                userSeries ?
+                                // ユーザープロフィールページの場合
+                                <UsersPagination
+                                    postsPerPage={postsPerPage}
+                                    totalPosts={items.length}
+                                    currentPage={currentPage}
+                                    setCurrentPage={setCurrentPage}
+                                /> :
+                                // その他の場合
+                                <Pagination
+                                    postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                    totalPosts={items.length} // 記事数
+                                    currentPage={currentPage}
+                                    paginateHref={paginateHref}
+                                />
+                            }
+                            <div className="display-multiple-items--items-wrapper">
+                                <SeriesWrapper items={currentItems} userId={userId} />
+                            </div>
+                            {
+                                userSeries ?
+                                <UsersPagination
+                                    postsPerPage={postsPerPage}
+                                    totalPosts={items.length}
+                                    currentPage={currentPage}
+                                    setCurrentPage={setCurrentPage}
+                                /> :
+                                <Pagination
+                                    postsPerPage={postsPerPage}  //1Pに表示する記事の数
+                                    totalPosts={items.length} // 記事数
+                                    currentPage={currentPage}
+                                    paginateHref={paginateHref}
+                                />
+                            }
+                        </React.Fragment>
                     }
                 </div>
             }
