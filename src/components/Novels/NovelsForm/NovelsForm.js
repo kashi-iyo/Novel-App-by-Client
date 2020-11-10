ab01fd79815c924bdd9aba2ee0dad2bb8fc3dbc5
kasi-iyo <snakeman143@gmail.com>
2020-11-10 23:27:52 +0900

    字数のバリデーション、ボタンをコンポーネント化など

--- src/components/Novels/NovelsForm/NovelsForm.js
+++ src/components/Novels/NovelsForm/NovelsForm.js
@@ -6,129 +6,136 @@ import '../../Series/Series.css'
 import './NovelsForm.css'
 import useItemsInput from '../../../CustomHooks/NovelsHooks/useItemsInput'
 import Flash from '../../Flash/Flash'
+import ValidateWordsCounts from '../../ValidateWordsCounts/ValidateWordsCounts'
+import Button from '../../Button/Button'
 
 // 小説作成フォームを作成
-function NovelsForm({ url, method, formType, dataType, novels, history, button, seriesId, novelsId }) {
-    // method: HTTPリクエスト, url: Railsのルーティング, sendItems: EditItemsから渡されるデータ
-    // props: historyなどの取得のため
-    const { values, release, itemSuccess, itemErrors, handleChange, handleSubmit, handleStatusChange, } =
+function NovelsForm({ url, method, formType, dataType, novels, history, button, seriesId, novelsId, handleFlashMessages }) {
+    const { values, release,  handleChange, handleSubmit, handleStatusChange, } =
         useItemsInput({
-            url: url,
-            method: method,
-            formType: formType,
-            dataType: dataType,
-            sendItems: novels,
-            history: history,
+            url: url,   //Railsへのルーティング
+            method: method, // HTTPリクエスト
+            formType: formType, // create or edit
+            dataType: dataType, // series or novel
+            sendItems: novels, // 編集用の小説データ
+            history: history,  // リダイレクトに使用
+            handleFlashMessages: handleFlashMessages  // フラッシュメッセージの表示
         })
 
     const title = values.novel_title
     const description = values.novel_description
     const content = values.novel_content
-    const tLength = title ? title.length : 0
-    const dLength = description ? description.length : 0
-    const cLength = content ? content.length : 0
 
-    // フィールドに入力された字数によりクラス名を変更する
-    const titleClass = classNames("ok", { "over": tLength > 50, "no": tLength === 0 })
-    const descriptionClass = classNames("ok", { "over": dLength > 300 })
-    const contentClass = classNames("ok", { "no": cLength === 0 })
-    const buttonClass = classNames("button", { "noButton": tLength > 50 || tLength === 0 || dLength > 300 })
 
     return (
         <React.Fragment>
-            <Flash Success={itemSuccess} Errors={itemErrors} />
-            <div className="SeriesForm">
-            {/* ボタンの文字列によって表示を切り替える */}
-            <div className="FormHeader">
-                {
-                    button === "追加する" ?
-                        <h3>╋小説追加</h3> :
-                        <h3>╋小説編集</h3>
-                }
-                <div className="SeriesForm__SeriesPage">
-                    <Link to={`/novel_series/${seriesId}`}>シリーズ管理画面へ戻る</Link>
+            <div className="series-form">
+                {/* ボタンの文字列によって表示を切り替える */}
+                <div className="series-form--header">
+                    {button === "追加する" ?
+                        <h3 className="caption">小説追加</h3> :
+                        <h3 className="caption">小説編集</h3>}
+                    <div className="series-form--back-to-series-page">
+                        <Link to={`/novel_series/${seriesId}`}>シリーズ管理画面へ戻る</Link>
+                    </div>
+                    {formType === "edit" &&
+                        <div className="series-form--back-to-novels-page">
+                            <Link to={`/novel_series/${seriesId}/novels/${novelsId}`}>小説管理画面へ戻る</Link>
+                        </div>}
                 </div>
-                {formType === "edit" && <div className="SeriesForm__NovelsPage">
-                    <Link to={`/novel_series/${seriesId}/novels/${novelsId}`}>小説管理画面へ戻る</Link>
-                </div>}
-            </div>
+                {/* ボタンの文字列によってイベントを切り替える */}
+                <form>
+                    {/*タイトル */}
+                    <div className="series-form--title-wrapper">
+                        {!title && <span className="error">【入力必須】</span>}
+                        <label htmlFor="novels_title" className="series-form--title-label">タイトル</label>
+                        <ValidateWordsCounts
+                            wordsLength={title.length}
+                            upperLimit={50}
+                            isRequire={true}
+                        />
+                        <input
+                            type="text"
+                            placeholder="タイトル"
+                            id="novels_title"
+                            name="novel_title"
+                            className="series-form--title-input"
+                            value={title}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <br></br>
+                    {/* ========== */}
 
-            {/* ボタンの文字列によってイベントを切り替える */}
-            <form onSubmit={handleSubmit}>
+                    {/* 前書き */}
+                    <div className="series-form--description-wrapper">
+                        <label
+                            htmlFor="novels_description" className="series-form--description-label">
+                            前書き
+                        </label>
+                        <ValidateWordsCounts
+                            wordsLength={description.length}
+                            upperLimit={300}
+                            isRequire={false}
+                        />
+                        <textarea
+                            placeholder="前書き"
+                            id="novels_description"
+                            name="novel_description"
+                            className="series-form--description-input"
+                            value={description}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    {/* ========== */}
 
-                {/*タイトル */}
-                <div className="TitleWrapper">
-                    {tLength === 0 && <span className={titleClass}>【入力必須】</span>}
-                    <label htmlFor="novels_title" className="Title">タイトル</label>
-                    <span className={titleClass}>
-                        {tLength}／50文字
-                        {tLength > 50 && <span className={titleClass}>【50文字以内で入力してください】</span> }
-                    </span>
-                    <input
-                        type="text"
-                        placeholder="タイトル"
-                        id="novels_title"
-                        name="novel_title"
-                        className="SeriesForm_title"
-                        value={title}
-                        onChange={handleChange}
+                    {/* 内容 */}
+                    <div className="series-form--content-wrapper">
+                        {!content && <span className="error">【入力必須】</span>}
+                        <label htmlFor="novels_content" className="series-form--content-label">
+                            本文
+                        </label>
+                        <ValidateWordsCounts
+                            wordsLength={content.length}
+                            isRequire={true}
+                        />
+                        <textarea
+                            placeholder="本文"
+                            id="novels_content"
+                            name="novel_content"
+                            className="series-form--content-input"
+                            value={content}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    {/* 公開チェックボックス */}
+                    <div className="release-wrapper">
+                        <input
+                            type="checkbox"
+                            name="release"
+                            id="release"
+                            className="release-input"
+                            checked={release}
+                            onChange={handleStatusChange}
+                        />
+                        <label htmlFor="release" className="release-label">公開する</label>
+                    </div>
+                    {/* ========== */}
+
+                    <Button
+                        handleSubmit={handleSubmit}
+                        badCase={
+                            title.length === 0 ||
+                            title.length > 50 ||
+                            description.length > 300 ||
+                            content.length === 0 ?
+                            true :
+                            false
+                        }
+                        buttonValue={button}
                     />
-                </div>
-                <br></br>
-                {/* ========== */}
-
-                {/* 前書き */}
-                <div className="DescriptionWrapper">
-                    <label htmlFor="novels_description" className="Description">前書き</label>
-                    <span className={descriptionClass}>
-                        {dLength}／300文字
-                        { dLength > 300 && <span className={descriptionClass}>【300文字以内で入力してください】</span> }
-                    </span>
-                    <textarea
-                        placeholder="前書き"
-                        id="novels_description"
-                        name="novel_description"
-                        className="SeriesForm_description"
-                        value={description}
-                        onChange={handleChange}
-                    />
-                </div>
-                {/* ========== */}
-
-                {/* 内容 */}
-                <div className="ContentWrapper">
-                    {!cLength && <span className={contentClass}>【入力必須】</span>}
-                    <label htmlFor="novels_content" className="Content">
-                        本文
-                    </label>
-                    <span className={contentClass}>
-                        {cLength}文字
-                    </span>
-                    <textarea
-                        placeholder="本文"
-                        id="novels_content"
-                        name="novel_content"
-                        className="content"
-                        value={content}
-                        onChange={handleChange}
-                    />
-                </div>
-
-                {/* 公開チェックボックス */}
-                <div className="releaseWrapper">
-                    <input
-                        type="checkbox"
-                        name="release"
-                        id="release"
-                        className="release"
-                        checked={release}
-                        onChange={handleStatusChange}
-                    />
-                    <label htmlFor="release" className="releaseLabel">公開する</label>
-                </div>
-                {/* ========== */}
-                <button type="submit" className={buttonClass}>{button}</button>
-            </form>
+                </form>
             </div>
         </React.Fragment>
     )
