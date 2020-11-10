52ac5896d4c56ce51e5604891bd49ca747ee41bf
kasi-iyo <snakeman143@gmail.com>
2020-11-10 23:29:35 +0900

    整理

--- src/components/Series/SeriesEdit/SeriesEdit.js
+++ src/components/Series/SeriesEdit/SeriesEdit.js
@@ -1,57 +1,37 @@
 import React from 'react'
 import './SeriesEdit'
 import SeriesForm from '../SeriesForm/SeriesForm'
-import ErrorMessages from '../../ErrorMessages/ErrorMessages'
 import useFetchEditItems from '../../../CustomHooks/NovelsHooks/useFetchEditItems'
 import Spinner from '../../Spinner/Spinner'
 
 // useEditItemsで取得したデータを、SeriesFormへを渡す
-function SeriesEdit({seriesId, history, currentUser, userId}) {
-    const { items, errors, isLoading } = useFetchEditItems({
+function SeriesEdit({seriesId, history, currentUser, handleFlashMessages}) {
+    const { items, isLoading } = useFetchEditItems({
         method: "get",
         url: `http://localhost:3001/api/v1/novel_series/${seriesId}/edit`,
-        history: history
+        history: history,
+        handleFlashMessages: handleFlashMessages
     })
 
-    // 編集フォームをレンダリングする
-    const seriesEditForm = () => {
-        return (
-            <div className='seriesEdit'>
-
-                {/* 小説一覧 */}
-                {/* <Link to="/novels">このシリーズの小説一覧</Link><br></br> */}
-                {
-                    items &&
-                        <SeriesForm
-                            editSeries={items}
-                            editTags={items.series_tags}
-                            method="patch"
-                            url={`http://localhost:3001/api/v1/novel_series/${seriesId}`}
-                            seriesId={seriesId}
-                            formType="edit"
-                            dataType="series"
-                            button="編集を完了する"
-                            history={history}
-                            currentUser={currentUser}
-                        />
-                }
-            </div>
-        )
-    }
-
-    const renderer = () => {
-        if (userId === items.user_id) {
-            return seriesEditForm()
-        } else if (errors) {
-            return <ErrorMessages errors={errors} />
-        }
-    }
-
     return (
-        <div>
-            {/* ログインし、尚且つエラーの存在しない場合に、編集フォームを表示 */}
-            {isLoading ? <Spinner /> : renderer()}
-        </div>
+        <React.Fragment>
+            {isLoading ? <Spinner /> :
+                items &&
+                <SeriesForm
+                    editSeries={items}
+                    editTags={items.series_tags}
+                    method="patch"
+                    url={`http://localhost:3001/api/v1/novel_series/${seriesId}`}
+                    seriesId={seriesId}
+                    formType="edit"
+                    dataType="series"
+                    button="編集を完了する"
+                    history={history}
+                    currentUser={currentUser}
+                    handleFlashMessages={handleFlashMessages}
+                />
+            }
+        </React.Fragment>
     )
 }
 
