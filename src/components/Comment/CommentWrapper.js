4782f926dde48769c7dbeec0c2dc0def3ae485c5
kasi-iyo <snakeman143@gmail.com>
2020-11-11 22:36:04 +0900

    ・コメントのステートは親コンポーネントで管理している。なので、コメント部分のステートを変更するには親コンポーネントからステート変更用の関数を流してくる必要がある。この関数がsetCommentItemsで、この関数の変数であるcommentItemsも同様に流してくる。 ・handleFlashMessagesは、イベント発火後にフラッシュメッセージを表示させるのに使用する ・クラス名の変更など

--- src/components/Comment/CommentWrapper.js
+++ src/components/Comment/CommentWrapper.js
@@ -5,43 +5,44 @@ import OneCommentWrapper from './OneCommentWrapper'
 import classNames from 'classnames'
 
 // コメント機能
-function CommentWrapper({ commentsObject, novelId, userId, currentUser }) {
+function CommentWrapper({ commentItems, setCommentItems, novelId, userId, currentUser, handleFlashMessages }) {
+    const commentCounts = commentItems.commentCounts
 
     // クリックしたときにコメント一覧を表示させる
     const [on, setOn] = useState(false)
 
     // content, handleChange, handleSubitなどフォームで扱う関数
-    const { commentsItems, setCommentsItems, content, success, errors, setSuccess, handleChange, handleSubmit } = useComment({
+    const { content, handleChange, handleSubmit } = useComment({
         novelId: novelId,   //バックエンドに送信するデータ
         userId: userId,     //バックエンドに送信するデータ
         currentUser: currentUser,       //バックエンドに送信するデータ
-        commentsCount: commentsObject.comments_count,     // コメント数の初期値
-        commentsUser: commentsObject.comments           // コメントしたユーザー
+        commentItems: commentItems,
+        setCommentItems: setCommentItems,
+        handleFlashMessages: handleFlashMessages
     })
 
     const buttonClass = classNames("button", { "noButton": content.length === 0  })
 
     const handleOn = () => {
-        if (on) {
-            setOn(false)
-        } else if (!on) {
-            setOn(true)
-        }
+        setOn(!on)
     }
 
     return (
     <React.Fragment>
-        <div className="NovelsContents__Comments">
-            <button type="submit" className="DisplayComment" onClick={handleOn}>
-                <p className="Comments__count">コメント：
-                    {commentsItems.commentsCount}
+        <div className="comment-wrapper--comments">
+            <button
+                type="submit"
+                className="comment-wrapper--down-button"
+                onClick={handleOn}
+            >
+                <p className="comment-wrapper--comment-counts">
+                    コメント： {commentCounts}
                 </p>
                 <span className="sankaku">▼</span>
             </button>
             {on &&
-                <ul className="Comments__Ul">
-                    {errors && <p className="error">{errors}</p>}
-                    <div className="CommentForm">
+                <ul className="comment-wrapper--comment-form-wrapper">
+                    <div className="comment-wrapper--comment-form">
                         <form onSubmit={e => handleSubmit(e)}>
                             <input
                                 type="text"
@@ -59,15 +60,12 @@ function CommentWrapper({ commentsObject, novelId, userId, currentUser }) {
                             />
                         </form>
                     </div>
-                    {success && <span className="success">{success}</span>}
-                    {commentsItems &&
-                        <OneCommentWrapper
-                            userId={userId}
-                            commentsItems={commentsItems}
-                            setCommentsItems={setCommentsItems}
-                            setSuccess={setSuccess}
-                        />
-                    }
+                    <OneCommentWrapper
+                        userId={userId}
+                        commentItems={commentItems}
+                        setCommentItems={setCommentItems}
+                        handleFlashMessages={handleFlashMessages}
+                    />
                 </ul>
             }
             </div>
