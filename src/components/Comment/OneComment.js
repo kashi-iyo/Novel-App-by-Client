0cc576b1cfb615fe9eb1a574e805b67adf3aaf9f
kasi-iyo <snakeman143@gmail.com>
2020-11-11 22:36:56 +0900

    ・コメント削除した後にコメント数とコメント内容を画面から消すために、親コンポーネントからコメントのステートを管理しているsetCommentItems関数を流してくる

--- src/components/Comment/OneComment.js
+++ src/components/Comment/OneComment.js
@@ -4,36 +4,35 @@ import useComment from '../../CustomHooks/CommentHooks/useComment'
 import './Comment.css'
 
 // 1つのコメントを表示
-function OneComment({ index, content, commentUser, userId, commentUserId, commentId, commentNovelId, commentsItems, setCommentsItems, setSuccess })
+function OneComment({ index, content, commentUser, userId, commentUserId, commentId, commentNovelId, commentItems, setCommentItems, handleFlashMessages })
 {
-
+console.log()
     // コメントの順番
     const indexNo = parseInt(index) + 1
 
     // 削除用の関数とサクセスメッセージ
     const { handleRemove } = useComment({
-        commentsCount: commentsItems.commentsCount,  // カウントの初期値
-        commentsUser: commentsItems.commentsUser,    //  コメントデータの初期値
-        setAfterRemove: setCommentsItems, // コメント削除後にStateを変更するのに使う
-        setRemoveSuccess: setSuccess    // コメント削除時に表示するメッセージに使う
+        commentItems: commentItems,
+        setCommentItems: setCommentItems,
+        handleFlashMessages: handleFlashMessages
     })
 
     return (
         <React.Fragment>
-            <div className="Comment">
-                <li className="Comment__List">
-                    <span className="indexNo">{indexNo} . </span>
+            <div className="one-comment">
+                <li className="one-comment--list">
+                    <span className="one-comment--index">{indexNo} . </span>
                     <Link
                         to={`/users/${commentUserId}`}
-                        className="Comment__commenter">
+                        className="one-comment--commenter">
                             {commentUser}
                     </Link>さん ：
-                    <span className="Comment__content">{content}</span>
+                    <span className="one-comment--content">{content}</span>
                     {userId === commentUserId &&
                         <span
                             type="submit"
                             onClick={() => handleRemove(commentNovelId, commentId)}
-                            className="CommentRemove"
+                            className="one-comment--remove-button"
                         >
                             削除
                     </span>
