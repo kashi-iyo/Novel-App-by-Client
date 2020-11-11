f9b675e8994a6c82c8c3f08cfbf7796c973c4522
kasi-iyo <snakeman143@gmail.com>
2020-11-11 22:37:12 +0900

    ・少しした整理

--- src/components/Comment/OneCommentWrapper.js
+++ src/components/Comment/OneCommentWrapper.js
@@ -2,31 +2,30 @@ import React from 'react'
 import OneComment from './OneComment'
 
 // OneCommentへデータを渡すためのコンポーネント
-function OneCommentWrapper({ userId, commentsItems, setCommentsItems, setSuccess}) {
-    const arr = commentsItems.commentsUser
+function OneCommentWrapper({ userId, commentItems, setCommentItems, handleFlashMessages }) {
+    const contents = commentItems.commentContents
 
     return (
         <React.Fragment>
-            {arr ?
-                Object.keys(arr).map(key => {
-                    let a = arr[key]
+            {contents.length !== 0 ?
+                Object.keys(contents).map(key => {
                     return (
                         <OneComment
                             key={key}
                             index={key}
                             userId={userId}
-                            commentId={a.comment_id}
-                            commentNovelId={a.comment_novel_id}
-                            commentUserId={a.comment_user_id}
-                            content={a.content}
-                            commentUser={a.commenter}
-                            commentsItems={commentsItems}
-                            setCommentsItems={setCommentsItems}
-                            setSuccess={setSuccess}
+                            commentId={contents[key].comment_id}
+                            commentNovelId={contents[key].comment_novel_id}
+                            commentUserId={contents[key].comment_user_id}
+                            content={contents[key].content}
+                            commentUser={contents[key].commenter}
+                            commentItems={commentItems}
+                            setCommentItems={setCommentItems}
+                            handleFlashMessages={handleFlashMessages}
                         />
                     )
                 }) :
-                <p>コメントはありません。</p>
+                <p>投稿されたコメントはありません。</p>
             }
         </React.Fragment>
     )
